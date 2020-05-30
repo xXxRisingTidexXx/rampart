7ba1cca4812930a01cfae48cc540661c910b2148
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-30 19:09:20 +0300

    Finished domria search struct description

--- pkg/mining/domria/item.go
+++ pkg/mining/domria/item.go
@@ -1,6 +1,20 @@
 package domria
 
 type item struct {
-	TotalArea float64
-
+	BeautifulURL        string            `json:"beautiful_url"`
+	MainPhoto           string            `json:"main_photo"`
+	PriceArr            map[string]string `json:"priceArr"`
+	TotalSquareMeters   float64           `json:"total_square_meters"`
+	LivingSquareMeters  float64           `json:"living_square_meters"`
+	KitchenSquareMeters float64           `json:"kitchen_square_meters"`
+	RoomsCount          int               `json:"rooms_count"`
+	Floor               int               `json:"floor"`
+	FloorsCount         int               `json:"floors_count"`
+	UserNewbuildNameUK  string            `json:"user_newbuild_name_uk"`
+	Longitude           coordinate        `json:"longitude"`
+	Latitude            coordinate        `json:"latitude"`
+	DistrictNameUK      string            `json:"district_name_uk"`
+	StreetNameUK        string            `json:"street_name_uk"`
+	StreetName          string            `json:"street_name"`
+	BuildingNumberStr   string            `json:"building_number_str"`
 }
--- pkg/mining/domria/search.go
+++ pkg/mining/domria/search.go
@@ -11,4 +11,3 @@ type search struct {
 func (search *search) String() string {
 	return fmt.Sprintf("{%v}", search.Items)
 }
-
