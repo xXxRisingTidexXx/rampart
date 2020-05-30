62eaaeacb4fa58ab6c640a6fbe0ce12639e85bca
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-30 21:43:37 +0300

    Extended flat field set

--- pkg/mining/domria/flat.go
+++ pkg/mining/domria/flat.go
@@ -1,8 +1,28 @@
 package domria
 
+import (
+	"rampart/pkg/mining"
+	"time"
+)
+
 type flat struct {
-	originURL string
-	imageURL  string
-	price     float64
-	totalArea float64
+	originURL   string
+	imageURL    string
+	accessTime  time.Time
+	price       float64
+	totalArea   float64
+	livingArea  float64
+	kitchenArea float64
+	roomNumber  int
+	floor       int
+	totalFloor  int
+	housing     mining.Housing
+	complex     string
+	longitude   float64  // TODO: check golang gis geometry library integration
+	latitude    float64
+	state       string
+	city        string
+	district    string
+	street      string
+	houseNumber string
 }
