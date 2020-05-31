9b7b4043bb25dee4109e068b14dc76bb77727e6a
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-31 14:06:51 +0300

    Moved magic ints to constants

--- pkg/mining/domria/moment.go
+++ pkg/mining/domria/moment.go
@@ -9,22 +9,29 @@ import (
 
 type moment time.Time
 
+const (
+	momentLength = 3
+	dateTimingLength = 2
+	dateLength = 3
+	timingLength = 3
+)
+
 func (m *moment) UnmarshalJSON(bytes []byte) error {
 	length := len(bytes)
-	if length < 3 {
+	if length < momentLength {
 		return fmt.Errorf("domria: moment string is too short, %d", length)
 	}
 	s := string(bytes[1 : length-1])
 	dateTiming := strings.Split(s, " ")
-	if len(dateTiming) != 2 {
+	if len(dateTiming) != dateTimingLength {
 		return fmt.Errorf("domria: moment can't split date & timing, %s", s)
 	}
 	date := strings.Split(dateTiming[0], "-")
-	if len(date) != 3 {
+	if len(date) != dateLength {
 		return fmt.Errorf("domria: moment cannot split date, %s", s)
 	}
 	timing := strings.Split(dateTiming[1], ":")
-	if len(timing) != 3 {
+	if len(timing) != timingLength {
 		return fmt.Errorf("domria: moment cannot split timing, %s", s)
 	}
 	year, err := strconv.Atoi(date[0])
