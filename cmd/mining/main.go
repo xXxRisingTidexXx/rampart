bd2477f956afe3e0212873a7aa374cb65e942332
xXxRisingTidexXx <danylopetrik@google.com>
2020-05-31 19:23:31 +0300

    Added config loading

--- cmd/mining/main.go
+++ cmd/mining/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
-	"rampart/pkg/mining"
-	"rampart/pkg/mining/domria"
+	"rampart/pkg/mining/config"
 )
 
 func main() {
 	log.SetLevel(log.DebugLevel)
 	log.Debug("mining: started")
-	prospector := domria.NewProspector(mining.Secondary)
-	if err := prospector.Prospect(); err != nil {
+	mining, err := config.NewMining()
+	if err != nil {
 		log.Fatal(err)
 	}
+	log.Info(mining)
+	//prospector := domria.NewProspector(mining.Secondary)
+	//if err := prospector.Prospect(); err != nil {
+	//	log.Fatal(err)
+	//}
 	log.Debug("mining: finished")
 }
--- pkg/mining/config/mining.go
+++ pkg/mining/config/mining.go
@@ -2,7 +2,38 @@ package config
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
 )
+
+func NewMining() (*Mining, error) {
+	_, filePath, _, ok := runtime.Caller(0)
+	if !ok {
+		return nil, fmt.Errorf("config: failed to find the caller path")
+	}
+	file, err := os.Open(
+		filepath.Join(
+			filepath.Dir(filepath.Dir(filepath.Dir(filepath.Dir(filePath)))),
+			"config",
+			"mining.yaml",
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("config: failed to open the config file, %v", err)
+	}
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("config: failed to read the config file, %v", err)
+	}
+	var mining Mining
+	if err = yaml.Unmarshal(bytes, &mining); err != nil {
+		return nil, fmt.Errorf("config: failed to unmarshal the config file, %v", err)
+	}
+	return &mining, nil
+}
 
 type Mining struct {
 	UserAgent   string       `yaml:"userAgent"`
