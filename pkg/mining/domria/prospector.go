feed09a8238ecee4dbd5cdfe420672023bb4b377
xXxRisingTidexXx <danylopetrik@google.com>
2020-06-01 10:40:02 +0300

    Injected validator

--- pkg/mining/domria/prospector.go
+++ pkg/mining/domria/prospector.go
@@ -7,19 +7,27 @@ import (
 )
 
 func NewProspector(housing mining.Housing, userAgent string, config *configs.Domria) mining.Prospector {
-	return &prospector{housing, newFetcher(userAgent, config.Fetcher)}
+	return &prospector{housing, newFetcher(userAgent, config.Fetcher), newValidator(config.Validator)}
 }
 
 type prospector struct {
-	housing mining.Housing
-	fetcher *fetcher
+	housing   mining.Housing
+	fetcher   *fetcher
+	validator *validator
 }
 
 func (prospector *prospector) Prospect() error {
 	log.Debugf("domria: %s housing prospector started", prospector.housing)
-	_, err := prospector.fetcher.fetchFlats(prospector.housing)
+	flats, err := prospector.fetcher.fetchFlats(prospector.housing)
 	if err != nil {
 		return err
+	}
+	if len(flats) == 0 {
+		return nil
+	}
+	flats = prospector.validator.validateFlats(flats)
+	if len(flats) == 0 {
+		return nil
 	}
 	log.Debugf("domria: %s housing prospector finished", prospector.housing)
 	return nil
