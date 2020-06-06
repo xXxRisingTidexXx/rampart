e11c57b3ef697e717d0da64f3964efb01ad2c567
xXxRisingTidexXx <danylopetrik@google.com>
2020-06-06 16:27:33 +0300

    Adapted fetcher spec

--- internal/mining/domria/fetcher_test.go
+++ internal/mining/domria/fetcher_test.go
@@ -29,13 +29,6 @@ func newDefaultFetcher() *fetcher {
 			Flags:           map[mining.Housing]string{mining.Primary: "pm_housing=1"},
 			Headers:         map[string]string{"User-Agent": "domria-test-bot/v1.0.0"},
 			SearchURL:       "https://domria.ua/search/",
-			OriginURLPrefix: "https://domria.ua/uk/",
-			ImageURLPrefix:  "https://cdn.riastatic.ua/photos/",
-			StateEnding:     "ька",
-			StateSuffix:     " область",
-			DistrictLabel:   "Район",
-			DistrictEnding:  "ий",
-			DistrictSuffix:  " район",
 		},
 	)
 }
