562e919815b8ae766d43969a6e517c8e5f3e71b4
xXxRisingTidexXx <danylopetrik@google.com>
2020-06-09 10:24:58 +0300

    Added geocoding logic

--- internal/mining/domria/geocoder.go
+++ internal/mining/domria/geocoder.go
@@ -2,6 +2,7 @@ package domria
 
 import (
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func newGeocoder() *geocoder {
@@ -23,19 +24,39 @@ func (geocoder *geocoder) geocodeFlats(flats []*flat) []*flat {
 		log.Debug("domria: geocoder skipped flats")
 		return flats
 	}
-	geocodedNumber, locatedNumber, avgDuration := 0, 0, 0.0
+	geocodedNumber, locatedNumber, duration := 0, 0, 0.0
 	newFlats := make([]*flat, 0, expectedLength)
-
+	for _, flat := range flats {
+		if flat.point != nil {
+			newFlats = append(newFlats, flat)
+		} else {
+			geocodedNumber++
+			start := time.Now()
+			newFlat, err := geocoder.geocodeFlat(flat)
+			duration += time.Since(start).Seconds()
+			if err != nil {
+				log.Error(err)
+			}
+			if newFlat != nil {
+				locatedNumber++
+				newFlats = append(newFlats, newFlat)
+			}
+		}
+	}
 	actualLength := len(newFlats)
 	log.Debugf(
-		"domria: geocoder passed %d flats; %d geocoded, %d located (%.3f)",
+		"domria: geocoder passed %d flats; %d geocoded, %d located (%.3fs)",
 		actualLength,
 		geocodedNumber,
 		locatedNumber,
-		avgDuration / float64(geocodedNumber),
+		duration/float64(geocodedNumber),
 	)
 	if lookup := float64(locatedNumber) / float64(geocodedNumber); lookup < geocoder.minLookup {
 		log.Warningf("domria: geocoder met low lookup (%.3f)", lookup)
 	}
 	return newFlats
+}
+
+func (geocoder *geocoder) geocodeFlat(f *flat) (*flat, error) {
+	return f, nil
 }
