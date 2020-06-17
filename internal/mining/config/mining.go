6299d3a9e8c7d77cb0e90562bf700b291edf832b
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-06-17 15:01:21 +0300

    Added & injected homedir path resolving

--- internal/mining/config/mining.go
+++ internal/mining/config/mining.go
@@ -2,51 +2,21 @@ package config
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
-	"os"
-	"path/filepath"
-	"runtime"
+	"rampart/internal/homedir"
 )
 
-func NewMining() (mining *Mining, err error) {
-	_, filePath, _, ok := runtime.Caller(0)
-	if !ok {
-		return nil, fmt.Errorf("config: mining failed to find the caller path")
-	}
-	file, err := os.Open(
-		filepath.Join(
-			filepath.Dir(filepath.Dir(filepath.Dir(filepath.Dir(filePath)))),
-			"config",
-			"mining.yaml",
-		),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("config: mining failed to open the config file, %v", err)
-	}
-	defer func() {
-		if closingErr := file.Close(); closingErr != nil {
-			closingErr = fmt.Errorf("config: mining failed to close the config file, %v", closingErr)
-			if err == nil {
-				err = closingErr
-			} else {
-				log.Error(closingErr)
-			}
-		}
-		if err != nil {
-			mining = nil
-		}
-	}()
-	bytes, err := ioutil.ReadAll(file)
+func NewMining() (*Mining, error) {
+	bytes, err := ioutil.ReadFile(homedir.Resolve("config/mining.yaml"))
 	if err != nil {
 		return nil, fmt.Errorf("config: mining failed to read the config file, %v", err)
 	}
-	var config Mining
-	if err = yaml.Unmarshal(bytes, &config); err != nil {
+	var mining Mining
+	if err = yaml.Unmarshal(bytes, &mining); err != nil {
 		return nil, fmt.Errorf("config: mining failed to unmarshal the config file, %v", err)
 	}
-	return &config, nil
+	return &mining, nil
 }
 
 type Mining struct {
