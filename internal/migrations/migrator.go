49569543f98d0d5fe4375146ceb84eb155b9a5f7
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-06-19 01:06:15 +0300

    Finished migration application

--- internal/migrations/migrator.go
+++ internal/migrations/migrator.go
@@ -37,9 +37,9 @@ func (migrator *migrator) ensureVersions() (int64, error) {
 	}
 	id := int64(0)
 	if count == 0 {
-		if _, err = migrator.tx.Exec("insert into versions values ($1)", id); err != nil {
+		if _, err := migrator.tx.Exec("insert into versions values ($1)", id); err != nil {
 			_ = migrator.tx.Rollback()
-			return 0, fmt.Errorf("migrations: migrator failed to insert the zero version, %v", err)
+			return 0, fmt.Errorf("migrations: migrator failed to create version %d, %v", id, err)
 		}
 	} else {
 		if err = migrator.tx.QueryRow("select id from versions").Scan(&id); err != nil {
@@ -52,12 +52,29 @@ func (migrator *migrator) ensureVersions() (int64, error) {
 }
 
 func (migrator *migrator) applyVersion(version *version) error {
+	start := time.Now()
+	query, err := version.load()
+	if err != nil {
+		_ = migrator.tx.Rollback()
+		return err
+	}
+	if _, err = migrator.tx.Exec(query); err != nil {
+		_ = migrator.tx.Rollback()
+		return fmt.Errorf("migrations: migrator failed to apply version %d, %v", version.id, err)
+	}
+	if _, err := migrator.tx.Exec("update versions set id = $1", version.id); err != nil {
+		_ = migrator.tx.Rollback()
+		return fmt.Errorf("migrations: migrator failed to update version %d, %v", version.id, err)
+	}
+	log.Debugf("migrations: migrator applied version %d (%.3fs)", version.id, time.Since(start).Seconds())
 	return nil
 }
 
 func (migrator *migrator) commit() error {
+	start := time.Now()
 	if err := migrator.tx.Commit(); err != nil {
 		return fmt.Errorf("migrations: migrator failed to commit the changes, %v", err)
 	}
+	log.Debugf("migrations: migrator committed (%.3fs)", time.Since(start).Seconds())
 	return nil
 }
