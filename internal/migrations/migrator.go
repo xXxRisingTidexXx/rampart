db73881b92d52f5bb16ec251fb0527de011dc137
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-06-18 14:19:32 +0300

    Organized migration workflow

--- internal/migrations/migrator.go
+++ internal/migrations/migrator.go
@@ -7,42 +7,52 @@ import (
 	"time"
 )
 
-func newMigrator(db *sql.DB) *migrator {
-	return &migrator{db}
+// TODO: inject tx instead of db
+func newMigrator(db *sql.DB) (*migrator, error) {
+	return &migrator{db}, nil
 }
 
 type migrator struct {
 	db *sql.DB
 }
 
-func (migrator *migrator) ensureVersions() error {
+// TODO: add latest reading
+func (migrator *migrator) ensureVersions() (int64, error) {
 	start := time.Now()
 	tx, err := migrator.db.Begin()
 	if err != nil {
-		return fmt.Errorf("migrations: migrator failed to init the versions, %v", err)
+		return 0, fmt.Errorf("migrations: migrator failed to init the versions, %v", err)
 	}
 	if _, err = tx.Exec("create table if not exists versions(id bigint not null)"); err != nil {
 		_ = tx.Rollback()
-		return fmt.Errorf("migrations: migrator failed to create the versions, %v", err)
+		return 0, fmt.Errorf("migrations: migrator failed to create the versions, %v", err)
 	}
 	count := 0
 	if err = tx.QueryRow("select count(*) from versions").Scan(&count); err != nil {
 		_ = tx.Rollback()
-		return fmt.Errorf("migrations: migrator failed to read the versions, %v", err)
+		return 0, fmt.Errorf("migrations: migrator failed to read the versions, %v", err)
 	}
 	if 1 < count {
 		_ = tx.Rollback()
-		return fmt.Errorf("migrations: migrator got multiple rows in the versions, %d", count)
+		return 0, fmt.Errorf("migrations: migrator got multiple rows in the versions, %d", count)
 	}
 	if count == 0 {
 		if _, err = tx.Exec("insert into versions values (0)"); err != nil {
 			_ = tx.Rollback()
-			return fmt.Errorf("migrations: migrator failed to set the zero version, %v", err)
+			return 0, fmt.Errorf("migrations: migrator failed to set the zero version, %v", err)
 		}
 	}
 	if err = tx.Commit(); err != nil {
-		return fmt.Errorf("migrations: migrator failed to commit the versions, %v", err)
+		return 0, fmt.Errorf("migrations: migrator failed to commit the versions, %v", err)
 	}
 	log.Debugf("migrations: migrator ensured the versions (%.3fs)", time.Since(start).Seconds())
+	return 0, nil
+}
+
+func (migrator *migrator) applyVersion(version *version) error {
+	return nil
+}
+
+func (migrator *migrator) commit() error {
 	return nil
 }
--- internal/migrations/run.go
+++ internal/migrations/run.go
@@ -20,12 +20,33 @@ func Run() error {
 		_ = db.Close()
 		return fmt.Errorf("migrations: failed to ping the db, %v", err)
 	}
-	migrator := newMigrator(db)
-	if err = migrator.ensureVersions(); err != nil {
+	versions, err := listVersions()
+	if err != nil {
 		_ = db.Close()
 		return err
 	}
-
+	migrator, err := newMigrator(db)
+	if err != nil {
+		_ = db.Close()
+		return err
+	}
+	latest, err := migrator.ensureVersions()
+	if err != nil {
+		_ = db.Close()
+		return err
+	}
+	for _, version := range versions {
+		if version.id > latest {
+			if err = migrator.applyVersion(version); err != nil {
+				_ = db.Close()
+				return err
+			}
+		}
+	}
+	if err = migrator.commit(); err != nil {
+		_ = db.Close()
+		return err
+	}
 	if err = db.Close(); err != nil {
 		return fmt.Errorf("migrations: failed to close the db, %v", err)
 	}
