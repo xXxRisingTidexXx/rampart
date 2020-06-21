36c1e7cc91cc8f4862fc9c19381750f9a41b7538
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-06-21 16:29:30 +0300

    Added flag to distinct single-time and periodical mining

--- cmd/mining/main.go
+++ cmd/mining/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"rampart/internal/mining"
 )
 
 func main() {
+	isOnce := flag.Bool("once", false, "Execute a single workflow instead of the whole schedule")
+	flag.Parse()
 	log.SetLevel(log.DebugLevel)
 	log.Debug("main: mining started")
-	if err := mining.Run(); err != nil {
+	function := mining.Schedule
+	if *isOnce {
+		function = mining.Run
+	}
+	if err := function(); err != nil {
 		log.Fatal(err)
 	}
 	log.Debug("main: mining finished")
