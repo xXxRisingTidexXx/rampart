c86aff1d52b4de19969689048c962f9d4db71e09
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-06-23 20:54:14 +0300

    Moved setup logic to main

--- internal/migrations/run.go
+++ internal/migrations/run.go
@@ -1,54 +1,31 @@
 package migrations
 
 import (
-	"fmt"
-	"rampart/internal/config"
-	"rampart/internal/database"
-	"rampart/internal/secrets"
+	"database/sql"
 )
 
-func Run() error {
-	scr, err := secrets.NewSecrets()
-	if err != nil {
-		return err
-	}
-	cfg, err := config.NewConfig()
-	if err != nil {
-		return err
-	}
-	db, err := database.NewDatabase(scr.DSN, cfg.Migrations.DSNParams)
-	if err != nil {
-		return err
-	}
+func Run(db *sql.DB) error {
 	versions, err := listVersions()
 	if err != nil {
-		_ = db.Close()
 		return err
 	}
 	migrator, err := newMigrator(db)
 	if err != nil {
-		_ = db.Close()
 		return err
 	}
 	latest, err := migrator.ensureVersions()
 	if err != nil {
-		_ = db.Close()
 		return err
 	}
 	for _, version := range versions {
 		if version.id > latest {
 			if err = migrator.applyVersion(version); err != nil {
-				_ = db.Close()
 				return err
 			}
 		}
 	}
 	if err = migrator.commit(); err != nil {
-		_ = db.Close()
 		return err
-	}
-	if err = db.Close(); err != nil {
-		return fmt.Errorf("migrations: failed to close the db, %v", err)
 	}
 	return nil
 }
