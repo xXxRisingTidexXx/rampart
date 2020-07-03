c077c648c4e775ce88a64c187ced20936f4b5f0a
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-03 16:56:12 +0300

    Removed cron panic recover

--- cmd/mining/main.go
+++ cmd/mining/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	_ "github.com/lib/pq"
-	"github.com/robfig/cron/v3"
+	gocron "github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
@@ -42,18 +42,18 @@ func main() {
 	if *isOnce {
 		miner.Run()
 	} else {
-		scheduler := cron.New(cron.WithChain(cron.Recover(cron.DefaultLogger)))
-		if _, err = scheduler.AddJob(miner.Spec(), miner); err != nil {
+		cron := gocron.New()
+		if _, err = cron.AddJob(miner.Spec(), miner); err != nil {
 			_ = db.Close()
 			_ = gatherer.Unregister()
 			log.Fatalf("main: mining failed to schedule, %v", err)
 		}
 		metrics.RunServer(miner.Port(), cfg.Mining.Server)
-		scheduler.Start()
+		cron.Start()
 		signalChannel := make(chan os.Signal, 1)
 		signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
 		<-signalChannel
-		scheduler.Stop()
+		cron.Stop()
 	}
 	if err = gatherer.Unregister(); err != nil {
 		_ = db.Close()
