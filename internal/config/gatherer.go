4e8a466069a8b535b873e860227cd8c5291c3bd8
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-04 22:18:02 +0300

    Defined tracker list

--- internal/config/gatherer.go
+++ internal/config/gatherer.go
@@ -5,9 +5,48 @@ import (
 )
 
 type Gatherer struct {
-	MiningDurationTracker *HistogramTracker `yaml:"miningDurationTracker"`
+	FailedFetchingTracker      *CounterTracker   `yaml:"failedFetchingTracker"`
+	FetchingDurationTracker    *HistogramTracker `yaml:"fetchingDurationTracker"`
+	FetchedFlatsTracker        *CounterTracker   `yaml:"fetchedFlatsTracker"`
+	LocatedFlatsTracker        *CounterTracker   `yaml:"locatedFlatsTracker"`
+	UnlocatedFlatsTracker      *CounterTracker   `yaml:"unlocatedFlatsTracker"`
+	FailedGeocodingTracker     *CounterTracker   `yaml:"failedGeocodingTracker"`
+	EmptyGeocodingTracker      *CounterTracker   `yaml:"emptyGeocodingTracker"`
+	SuccessfulGeocodingTracker *CounterTracker   `yaml:"successfulGeocodingTracker"`
+	GeocodingDurationTracker   *HistogramTracker `yaml:"geocodingDurationTracker"`
+	ValidatedFlatsTracker      *CounterTracker   `yaml:"validatedFlatsTracker"`
+	InvalidatedFlatsTracker    *CounterTracker   `yaml:"invalidatedFlatsTracker"`
+	CreatedFlatsTracker        *CounterTracker   `yaml:"createdFlatsTracker"`
+	UpdatedFlatsTracker        *CounterTracker   `yaml:"updatedFlatsTracker"`
+	UnalteredFlatsTracker      *CounterTracker   `yaml:"unalteredFlatsTracker"`
+	FailedStoringTracker       *CounterTracker   `yaml:"failedStoringTracker"`
+	ReadingDurationTracker     *HistogramTracker `yaml:"readingDurationTracker"`
+	CreationDurationTracker    *HistogramTracker `yaml:"creationDurationTracker"`
+	UpdateDurationTracker      *HistogramTracker `yaml:"updateDurationTracker"`
+	MiningDurationTracker      *HistogramTracker `yaml:"miningDurationTracker"`
 }
 
 func (gatherer *Gatherer) String() string {
-	return fmt.Sprintf("{%v}", gatherer.MiningDurationTracker)
+	return fmt.Sprintf(
+		"{%v %v %v %v %v %v %v %v %v %v %v %v %v %v %v %v %v %v %v}",
+		gatherer.FailedFetchingTracker,
+		gatherer.FetchingDurationTracker,
+		gatherer.FetchedFlatsTracker,
+		gatherer.LocatedFlatsTracker,
+		gatherer.UnlocatedFlatsTracker,
+		gatherer.FailedGeocodingTracker,
+		gatherer.EmptyGeocodingTracker,
+		gatherer.SuccessfulGeocodingTracker,
+		gatherer.GeocodingDurationTracker,
+		gatherer.ValidatedFlatsTracker,
+		gatherer.InvalidatedFlatsTracker,
+		gatherer.CreatedFlatsTracker,
+		gatherer.UpdatedFlatsTracker,
+		gatherer.UnalteredFlatsTracker,
+		gatherer.FailedStoringTracker,
+		gatherer.ReadingDurationTracker,
+		gatherer.CreationDurationTracker,
+		gatherer.UpdateDurationTracker,
+		gatherer.MiningDurationTracker,
+	)
 }
