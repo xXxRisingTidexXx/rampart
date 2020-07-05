10b8fde842fbd94aecd0e8a5ed5fa883c6e859eb
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-05 20:29:40 +0300

    Added all tracker unregestering

--- internal/mining/metrics/gatherer.go
+++ internal/mining/metrics/gatherer.go
@@ -127,5 +127,59 @@ func (gatherer *Gatherer) GatherRunDuration(runDuration float64) {
 }
 
 func (gatherer *Gatherer) Unregister() error {
+	if err := gatherer.failedFetchingTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.fetchingDurationTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.fetchedFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.locatedFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.unlocatedFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.failedGeocodingTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.emptyGeocodingTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.successfulGeocodingTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.geocodingDurationTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.validatedFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.invalidatedFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.createdFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.updatedFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.unalteredFlatsTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.failedStoringTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.readingDurationTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.creationDurationTracker.unregister(); err != nil {
+		return err
+	}
+	if err := gatherer.updateDurationTracker.unregister(); err != nil {
+		return err
+	}
 	return gatherer.runDurationTracker.unregister()
 }
