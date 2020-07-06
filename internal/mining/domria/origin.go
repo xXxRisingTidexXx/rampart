9866d09a708332c761c641ea65b32a1e09511d9c
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-06 12:45:31 +0300

    Restored origin

--- internal/mining/domria/origin.go
+++ internal/mining/domria/origin.go
@@ -7,9 +7,8 @@ import (
 
 type origin struct {
 	updateTime time.Time
-	price      float64
 }
 
 func (origin *origin) String() string {
-	return fmt.Sprintf("{%s %.1f}", origin.updateTime, origin.price)
+	return fmt.Sprintf("{%s}", origin.updateTime)
 }
