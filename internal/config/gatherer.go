d7aee854c4739bddd34eb4bd07d315d2071bee68
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-08 23:52:56 +0300

    Narrowed trackers to counter & histogram ones

--- internal/config/gatherer.go
+++ internal/config/gatherer.go
@@ -2,23 +2,34 @@ package config
 
 import (
 	"fmt"
+	"rampart/internal/misc"
 )
 
 type Gatherer struct {
-	RunTracker        *RunTracker        `yaml:"runTracker"`
-	GeocodingTracker  *GeocodingTracker  `yaml:"geocodingTracker"`
-	ValidationTracker *ValidationTracker `yaml:"validationTracker"`
-	StoringTracker    *StoringTracker    `yaml:"storingTracker"`
-	DurationTracker   *DurationTracker   `yaml:"durationTracker"`
+	RunTracker        *CounterTracker    `yaml:"runTracker"`
+	Statuses          *misc.Statuses     `yaml:"statuses"`
+	GeocodingTracker  *CounterTracker    `yaml:"geocodingTracker"`
+	Categories        *misc.Categories   `yaml:"categories"`
+	ValidationTracker *CounterTracker    `yaml:"validationTracker"`
+	Verdicts          *misc.Verdicts     `yaml:"verdicts"`
+	StoringTracker    *CounterTracker    `yaml:"storingTracker"`
+	Consequences      *misc.Consequences `yaml:"consequences"`
+	DurationTracker   *HistogramTracker  `yaml:"durationTracker"`
+	Processes         *misc.Processes    `yaml:"processes"`
 }
 
 func (gatherer *Gatherer) String() string {
 	return fmt.Sprintf(
-		"{%v %v %v %v %v}",
+		"{%v %v %v %v %v %v %v %v %v %v}",
 		gatherer.RunTracker,
+		gatherer.Statuses,
 		gatherer.GeocodingTracker,
+		gatherer.Categories,
 		gatherer.ValidationTracker,
+		gatherer.Verdicts,
 		gatherer.StoringTracker,
+		gatherer.Consequences,
 		gatherer.DurationTracker,
+		gatherer.Processes,
 	)
 }
