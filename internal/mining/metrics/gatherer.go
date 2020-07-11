a4e757bda6cd461d6b530429b720459d9063b10b
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-12 01:45:14 +0300

    Satisfied godot

--- internal/mining/metrics/gatherer.go
+++ internal/mining/metrics/gatherer.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// TODO: pass the registry to metrics server
+// TODO: pass the registry to metrics server.
 func NewGatherer(miner string, config *config.Gatherer) *Gatherer {
 	registry := prometheus.NewRegistry()
 	registry.MustRegister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}))
