883c697594f808c2d28c33e26ab235d141e9e1fd
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-07-19 23:50:41 +0300

    Simplified state sanitixation

--- internal/mining/domria/sanitizer.go
+++ internal/mining/domria/sanitizer.go
@@ -57,10 +57,7 @@ func (sanitizer *Sanitizer) sanitizeFlat(flat *Flat) *Flat {
 	}
 	state := strings.TrimSpace(flat.State)
 	if value, ok := sanitizer.stateDictionary[state]; ok {
-		state = value
-	}
-	if state != "" {
-		state += sanitizer.stateSuffix
+		state = value + sanitizer.stateSuffix
 	}
 	city := strings.TrimSpace(flat.City)
 	if value, ok := sanitizer.cityDictionary[city]; ok {
