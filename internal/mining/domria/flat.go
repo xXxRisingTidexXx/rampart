2dc43451604b72ecd440e870b78cbfa9bdaa391b
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-08-08 13:40:40 +0300

    Added zone distance fields to flats

--- internal/mining/domria/flat.go
+++ internal/mining/domria/flat.go
@@ -7,26 +7,28 @@ import (
 )
 
 type Flat struct {
-	OriginURL             string
-	ImageURL              string
-	UpdateTime            time.Time
-	Price                 float64
-	TotalArea             float64
-	LivingArea            float64
-	KitchenArea           float64
-	RoomNumber            int
-	Floor                 int
-	TotalFloor            int
-	Housing               string
-	Complex               string
-	Point                 orb.Point
-	SubwayStationDistance float64
-	State                 string
-	City                  string
-	District              string
-	Street                string
-	HouseNumber           string
-	Source                string
+	OriginURL              string
+	ImageURL               string
+	UpdateTime             time.Time
+	Price                  float64
+	TotalArea              float64
+	LivingArea             float64
+	KitchenArea            float64
+	RoomNumber             int
+	Floor                  int
+	TotalFloor             int
+	Housing                string
+	Complex                string
+	Point                  orb.Point
+	SubwayStationDistance  float64
+	IndustrialZoneDistance float64
+	GreenZoneDistance      float64
+	State                  string
+	City                   string
+	District               string
+	Street                 string
+	HouseNumber            string
+	Source                 string
 }
 
 func (flat *Flat) URL() string {
@@ -39,7 +41,7 @@ func (flat *Flat) Body() string {
 
 func (flat *Flat) String() string {
 	return fmt.Sprintf(
-		"{%s %s %s %.2f %.1f %.1f %.1f %d %d %d %s %s %v %.5f %s %s %s %s %s %s}",
+		"{%s %s %s %.2f %.1f %.1f %.1f %d %d %d %s %s %v %.5f %.5f %.5f %s %s %s %s %s %s}",
 		flat.OriginURL,
 		flat.ImageURL,
 		flat.UpdateTime,
@@ -54,6 +56,8 @@ func (flat *Flat) String() string {
 		flat.Complex,
 		flat.Point,
 		flat.SubwayStationDistance,
+		flat.IndustrialZoneDistance,
+		flat.GreenZoneDistance,
 		flat.State,
 		flat.City,
 		flat.District,
