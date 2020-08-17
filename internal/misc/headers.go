c22966a7c8bac80f1c56261faed41c0036056039
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-08-17 08:28:13 +0300

    Added apply method

--- internal/misc/headers.go
+++ internal/misc/headers.go
@@ -11,3 +11,9 @@ func (headers Headers) Inject(request *http.Request) {
 		request.Header.Set(key, value)
 	}
 }
+
+func (headers Headers) Apply(writer http.ResponseWriter)  {
+	for key, value := range headers {
+		writer.Header().Set(key, value)
+	}
+}
