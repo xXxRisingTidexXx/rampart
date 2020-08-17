0c7cba5f15915d6490a8f99fdf4f7b020ab9b9fd
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-08-17 16:31:38 +0300

    Removed pattern check

--- internal/gauging/handler.go
+++ internal/gauging/handler.go
@@ -12,10 +12,7 @@ func newHandler() *handler {
 type handler struct{}
 
 func (handler *handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	if url := request.URL.String(); url != "/" {
-		writer.WriteHeader(http.StatusNotFound)
-		log.Errorf("httpserve: received invalid request url, %s", url)
-	} else if request.Method != http.MethodPost {
+	if request.Method != http.MethodPost {
 		writer.WriteHeader(http.StatusMethodNotAllowed)
 		log.Errorf("httpserve: received invalid request method, %s", request.Method)
 	} else {
