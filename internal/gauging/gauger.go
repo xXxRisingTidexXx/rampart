5a412646c577ee939309ca07263f834199bcc4d7
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-08-18 16:41:13 +0300

    Added gauger loop

--- internal/gauging/gauger.go
+++ internal/gauging/gauger.go
@@ -7,13 +7,24 @@ import (
 )
 
 func NewGauger() *Gauger {
-	return &Gauger{&http.Client{Timeout: 35 * time.Second}}
+	gauger := &Gauger{&http.Client{Timeout: 35 * time.Second}, make(chan *dto.Location, 200)}
+	go gauger.run()
+	return gauger
 }
 
 type Gauger struct {
-	client *http.Client
+	client          *http.Client
+	locationChannel chan *dto.Location
+}
+
+func (gauger *Gauger) run() {
+	for range gauger.locationChannel {
+
+	}
 }
 
 func (gauger *Gauger) GaugeDistances(locations []*dto.Location) {
-
+	for _, location := range locations {
+		gauger.locationChannel <- location
+	}
 }
