bbdf698f1faaea7263238a823a98595fd332ef08
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-08-18 16:32:59 +0300

    Beautified gauging handler

--- internal/gauging/gauger.go
+++ internal/gauging/gauger.go
@@ -1,13 +1,19 @@
 package gauging
 
+import (
+	"github.com/xXxRisingTidexXx/rampart/internal/dto"
+	"net/http"
+	"time"
+)
+
 func NewGauger() *Gauger {
-	return &Gauger{}
+	return &Gauger{&http.Client{Timeout: 35 * time.Second}}
 }
 
 type Gauger struct {
-
+	client *http.Client
 }
 
-func (gauger *Gauger) GaugeDistances() {
+func (gauger *Gauger) GaugeDistances(locations []*dto.Location) {
 
 }
--- internal/gauging/handler.go
+++ internal/gauging/handler.go
@@ -1,7 +1,9 @@
 package gauging
 
 import (
+	"encoding/json"
 	log "github.com/sirupsen/logrus"
+	"github.com/xXxRisingTidexXx/rampart/internal/dto"
 	"net/http"
 )
 
@@ -16,8 +18,14 @@ type handler struct {
 func (handler *handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodPost {
 		writer.WriteHeader(http.StatusMethodNotAllowed)
-		log.Errorf("httpserve: received invalid request method, %s", request.Method)
-	} else {
-		log.Info("gauging: success")
+		log.Errorf("gauging: received invalid request method, %s", request.Method)
+		return
 	}
+	locations := make([]*dto.Location, 0)
+	if err := json.NewDecoder(request.Body).Decode(&locations); err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Errorf("gauging: failed to unmarshal the locations, %v", err)
+		return
+	}
+	handler.gauger.GaugeDistances(locations)
 }
