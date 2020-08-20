754e8fc3df5dddfbd2e1ee76f66798aed83b7433
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-08-20 22:14:08 +0300

    Added custom geo distance gauging

--- internal/gauging/distance_gauger.go
+++ internal/gauging/distance_gauger.go
@@ -4,18 +4,18 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/geo"
 	"github.com/paulmach/orb/geojson"
-	"github.com/paulmach/orb/planar"
 	"github.com/paulmach/osm"
 	"github.com/paulmach/osm/osmgeojson"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/dto"
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
-	"math"
 	"net/http"
 	gourl "net/url"
 )
 
+// TODO: transform min area square degrees into square meters.
 type distanceGauger struct {
 	client         *http.Client
 	headers        misc.Headers
@@ -56,24 +56,100 @@ func (gauger *distanceGauger) queryCollection(query string, params ...interface{
 	return collection, nil
 }
 
-func (gauger *distanceGauger) gaugeDistance(flat *dto.Flat, collection *geojson.FeatureCollection) float64 {
+func (gauger *distanceGauger) gaugeDistance(
+	flat *dto.Flat,
+	collection *geojson.FeatureCollection,
+) float64 {
 	features := make([]*geojson.Feature, 0)
 	for _, feature := range collection.Features {
-		if planar.Area(feature.Geometry) >= gauger.minArea {
+		if geo.Area(feature.Geometry) >= gauger.minArea {
 			features = append(features, feature)
 		}
 	}
-	if len(features) == 0 {
+	point, id, distance := orb.Point(flat.Point), "", misc.DistanceUnknown
+	for _, feature := range features {
+		newDistance := gauger.gaugeGeoDistance(feature.Geometry, point)
+		if gauger.isLower(newDistance, distance) {
+			if newID, ok := feature.ID.(string); ok {
+				id = newID
+			} else {
+				id = ""
+			}
+			distance = newDistance
+		}
+	}
+	log.WithFields(log.Fields{"point": point, "id": id, "distance": distance}).Info("gauging: gauged distance")
+	return distance
+}
+
+func (gauger *distanceGauger) isLower(distance1, distance2 float64) bool {
+	return distance1 < distance2 || distance2 == misc.DistanceUnknown && distance2 < distance1
+}
+
+func (gauger *distanceGauger) gaugeGeoDistance(geometry orb.Geometry, point orb.Point) float64 {
+	if geometry == nil {
 		return misc.DistanceUnknown
 	}
-	point := orb.Point(flat.Point)
-	id, distance := features[0].ID, planar.DistanceFrom(features[0].Geometry, point)
-	for _, feature := range features {
-		if planar.DistanceFrom(feature.Geometry, point) < distance {
-			id = feature.ID
+	switch geometry := geometry.(type) {
+	case orb.Point:
+		return geo.DistanceHaversine(geometry, point)
+	case orb.MultiPoint:
+		return gauger.gaugeGeoDistanceToPoints(geometry, point)
+	case orb.LineString:
+		return gauger.gaugeGeoDistanceToPoints(geometry, point)
+	case orb.MultiLineString:
+		distance := misc.DistanceUnknown
+		for _, lineString := range geometry {
+			newDistance := gauger.gaugeGeoDistanceToPoints(lineString, point)
+			if gauger.isLower(newDistance, distance) {
+				distance = newDistance
+			}
 		}
-		distance = math.Min(distance, planar.DistanceFrom(feature.Geometry, point))
+		return distance
+	case orb.Ring:
+		return gauger.gaugeGeoDistanceToPoints(geometry, point)
+	case orb.Polygon:
+		distance := misc.DistanceUnknown
+		for _, ring := range geometry {
+			newDistance := gauger.gaugeGeoDistanceToPoints(ring, point)
+			if gauger.isLower(newDistance, distance) {
+				distance = newDistance
+			}
+		}
+		return distance
+	case orb.MultiPolygon:
+		distance := misc.DistanceUnknown
+		for _, polygon := range geometry {
+			for _, ring := range polygon {
+				newDistance := gauger.gaugeGeoDistanceToPoints(ring, point)
+				if gauger.isLower(newDistance, distance) {
+					distance = newDistance
+				}
+			}
+		}
+		return distance
+	case orb.Collection:
+		distance := misc.DistanceUnknown
+		for _, newGeometry := range geometry {
+			newDistance := gauger.gaugeGeoDistance(newGeometry, point)
+			if gauger.isLower(newDistance, distance) {
+				distance = newDistance
+			}
+		}
+		return distance
+	case orb.Bound:
+		return gauger.gaugeGeoDistanceToPoints(geometry.ToRing(), point)
 	}
-	log.WithFields(log.Fields{"point": flat.Point, "id": id, "distance": distance}).Info("gauging: gauged distance")
+	return misc.DistanceUnknown
+}
+
+func (gauger *distanceGauger) gaugeGeoDistanceToPoints(points []orb.Point, point orb.Point) float64 {
+	distance := misc.DistanceUnknown
+	for _, newPoint := range points {
+		newDistance := geo.DistanceHaversine(newPoint, point)
+		if gauger.isLower(newDistance, distance) {
+			distance = newDistance
+		}
+	}
 	return distance
 }
