68ed4e2399f21432a267d4ce0d94b1944d9c28d1
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-09-04 11:43:53 +0300

    Added get env with emptiness check

--- internal/misc/shortcuts.go
+++ internal/misc/shortcuts.go
@@ -1,6 +1,8 @@
 package misc
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 )
@@ -17,4 +19,11 @@ var rootDir = ""
 
 func ResolvePath(path string) string {
 	return filepath.Join(rootDir, path)
+}
+
+func GetEnv(key string) (string, error) {
+	if value := os.Getenv(key); value != "" {
+		return value, nil
+	}
+	return "", fmt.Errorf("misc: failed to find the env %s", key)
 }
