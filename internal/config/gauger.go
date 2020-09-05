3022fe1983897303729ba28b07c3b6c654be52b4
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-09-06 01:15:25 +0300

    Upgraded gauger config struct

--- internal/config/gauger.go
+++ internal/config/gauger.go
@@ -7,18 +7,40 @@ import (
 )
 
 type Gauger struct {
-	Timeout        time.Duration `yaml:"timeout"`
-	Headers        misc.Headers  `yaml:"headers"`
-	InterpreterURL string        `yaml:"interpreterURL"`
-	SubwayCities   misc.Set      `yaml:"subwayCities"`
+	Timeout         time.Duration `yaml:"timeout"`
+	Headers         misc.Headers  `yaml:"headers"`
+	InterpreterURL  string        `yaml:"interpreterURL"`
+	SubwayCities    misc.Set      `yaml:"subwayCities"`
+	SSFSearchRadius float64       `yaml:"ssfSearchRadius"`
+	SSFMinDistance  float64       `yaml:"ssfMinDistance"`
+	SSFModifier     float64       `yaml:"ssfModifier"`
+	IZFSearchRadius float64       `yaml:"izfSearchRadius"`
+	IZFMinArea      float64       `yaml:"izfMinArea"`
+	IZFMinDistance  float64       `yaml:"izfMinDistance"`
+	IZFModifier     float64       `yaml:"izfModifier"`
+	GZFSearchRadius float64       `yaml:"gzfSearchRadius"`
+	GZFMinArea      float64       `yaml:"gzfMinArea"`
+	GZFMinDistance  float64       `yaml:"gzfMinDistance"`
+	GZFModifier     float64       `yaml:"gzfModifier"`
 }
 
 func (gauger *Gauger) String() string {
 	return fmt.Sprintf(
-		"{%s %v %s %v}",
+		"{%s %v %s %v %f %f %f %f %f %f %f %f %f %f %f}",
 		gauger.Timeout,
 		gauger.Headers,
 		gauger.InterpreterURL,
 		gauger.SubwayCities,
+		gauger.SSFSearchRadius,
+		gauger.SSFMinDistance,
+		gauger.SSFModifier,
+		gauger.IZFSearchRadius,
+		gauger.IZFMinArea,
+		gauger.IZFMinDistance,
+		gauger.IZFModifier,
+		gauger.GZFSearchRadius,
+		gauger.GZFMinArea,
+		gauger.GZFMinDistance,
+		gauger.GZFModifier,
 	)
 }
