7ed8413e55902d4d2bfa1ad6bd359c349fa5447f
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-09-07 11:26:38 +0300

    Removed uglydsn query params

--- internal/misc/shortcuts.go
+++ internal/misc/shortcuts.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq" // Postgres driver along with the DB opener.
-	gourl "net/url"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -31,20 +30,8 @@ func ResolvePath(path string) string {
 	return filepath.Join(rootDir, path)
 }
 
-func OpenDB(dsn string, params map[string]string) (*sql.DB, error) {
-	url, err := gourl.Parse(dsn)
-	if err != nil {
-		return nil, fmt.Errorf("misc: invalid dsn %s, %v", dsn, err)
-	}
-	values, err := gourl.ParseQuery(url.RawQuery)
-	if err != nil {
-		return nil, fmt.Errorf("misc: invalid dsn params %s, %v", dsn, err)
-	}
-	for key, value := range params {
-		values.Set(key, value)
-	}
-	url.RawQuery = values.Encode()
-	db, err := sql.Open("postgres", url.String())
+func OpenDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("misc: failed to open the db, %v", err)
 	}
