25721bc0baee85082bd1486fe0e891050b955847
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-02 02:21:04 +0300

    Upgraded todo formulas

--- cmd/mining/main.go
+++ cmd/mining/main.go
@@ -16,7 +16,8 @@ import (
 // TODO: replace config & flat passed types with values.
 // TODO: relative city center distance feature (with city diameter).
 // TODO: distance to workplace feature.
-// TODO: floor factor, f: floor, tf: total_floor; ff(f, tf) = tf / (max(tf - f - k, 0) + 1).
+// TODO: floor factor, f: floor, tf: total floor; ff = tf / (tf - f + 1).
+// TODO: price factor, ap: actual price, up: utmost price; pf = e ^ (1 - ap / up).
 func main() {
 	isDebug := flag.Bool("debug", false, "Execute a single workflow instead of the whole schedule")
 	alias := flag.String("miner", "", "Desired miner alias")
