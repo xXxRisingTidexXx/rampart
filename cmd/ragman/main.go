a9f2b5b895acd9b64260a38531cac057b821467e
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-08 17:38:51 +0300

    Upgraded ragman

--- cmd/ragman/main.go
+++ cmd/ragman/main.go
@@ -3,25 +3,22 @@ package main
 import (
 	gobytes "bytes"
 	"encoding/json"
-	"fmt"
 	gocron "github.com/robfig/cron/v3"
+	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 	"github.com/xXxRisingTidexXx/rampart/internal/mining/domria"
 	"github.com/xXxRisingTidexXx/rampart/internal/mining/metrics"
 	"io/ioutil"
-	"os"
 )
 
 func main() {
 	cfg, err := config.NewConfig()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	bytes, err := ioutil.ReadFile("datasets/outdated.txt")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	urls := map[string]struct{}{}
 	for _, line := range gobytes.Split(bytes, []byte{'\n'}) {
@@ -56,16 +53,16 @@ type Ragman struct {
 func (ragman *Ragman) Run() {
 	flats, err := ragman.fetcher.FetchFlats(ragman.housing)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		buffer := &gobytes.Buffer{}
-		for _, flat := range flats {
-			if _, ok := ragman.urls["https://dom.ria.com/uk/" + flat.OriginURL]; ok {
-				if err := json.Indent(buffer, []byte(flat.Source), "", "  "); err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println(buffer.String())
-				}
+		log.Error(err)
+		return
+	}
+	buffer := &gobytes.Buffer{}
+	for _, flat := range flats {
+		if _, ok := ragman.urls["https://dom.ria.com/uk/"+flat.OriginURL]; ok {
+			if err := json.Indent(buffer, []byte(flat.Source), "", "  "); err != nil {
+				log.Error(err)
+			} else {
+				log.Info(buffer.String())
 			}
 		}
 	}
