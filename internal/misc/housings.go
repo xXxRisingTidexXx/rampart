81a4faee018e0b04d6382d55aad8265ba6b212d8
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-12 22:43:32 +0300

    Added housing enum

--- internal/misc/housings.go
+++ internal/misc/housings.go
@@ -4,3 +4,22 @@ const (
 	HousingPrimary   = "primary"
 	HousingSecondary = "secondary"
 )
+
+type Housing int
+
+const (
+	PrimaryHousing Housing = iota
+	SecondaryHousing
+)
+
+var housingViews = map[Housing]string{
+	PrimaryHousing: "primary",
+	SecondaryHousing: "secondary",
+}
+
+func (housing Housing) String() string {
+	if view, ok := housingViews[housing]; ok {
+		return view
+	}
+	return "undefined"
+}
