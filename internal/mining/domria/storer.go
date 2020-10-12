7921c0aa905b2045ef5e970ffda72f96098c9a40
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-13 01:57:15 +0300

    Removed image url, renamed origin url and narrowed it down

--- internal/mining/domria/storer.go
+++ internal/mining/domria/storer.go
@@ -86,7 +86,7 @@ func (storer *Storer) storeFlat(flat Flat) error {
 }
 
 func (storer *Storer) readFlat(tx *sql.Tx, flat Flat) (origin, error) {
-	row := tx.QueryRow(`select update_time from flats where origin_url = $1`, flat.OriginURL)
+	row := tx.QueryRow(`select update_time from flats where url = $1`, flat.OriginURL)
 	o := origin{}
 	switch err := row.Scan(&o.updateTime); err {
 	case sql.ErrNoRows:
@@ -102,30 +102,28 @@ func (storer *Storer) readFlat(tx *sql.Tx, flat Flat) (origin, error) {
 func (storer *Storer) updateFlat(tx *sql.Tx, flat Flat) error {
 	_, err := tx.Exec(
 		`update flats 
-		set image_url = $2,
-		    update_time = $3,
+		set update_time = $2,
 		    parsing_time = now() at time zone 'utc',
-		    price = $4,
-		    total_area = $5,
-		    living_area = $6,
-		    kitchen_area = $7,
-		    room_number = $8,
-		    floor = $9,
-		    total_floor = $10,
-		    housing = $11,
-		    complex = $12,
-		    point = st_geomfromwkb($13, $14),
-		    state = $15,
-		    city = $16,
-		    district = $17,
-		    street = $18,
-		    house_number = $19,
-		    ssf = $20,
-		    izf = $21,
-		    gzf = $22
-		where origin_url = $1`,
+		    price = $3,
+		    total_area = $4,
+		    living_area = $5,
+		    kitchen_area = $6,
+		    room_number = $7,
+		    floor = $8,
+		    total_floor = $9,
+		    housing = $10,
+		    complex = $11,
+		    point = st_geomfromwkb($12, $13),
+		    state = $14,
+		    city = $15,
+		    district = $16,
+		    street = $17,
+		    house_number = $18,
+		    ssf = $19,
+		    izf = $20,
+		    gzf = $21
+		where url = $1`,
 		flat.OriginURL,
-		flat.ImageURL,
 		flat.UpdateTime,
 		flat.Price,
 		flat.TotalArea,
@@ -157,17 +155,16 @@ func (storer *Storer) createFlat(tx *sql.Tx, flat Flat) error {
 	_, err := tx.Exec(
 		`insert into flats
         (
-         	origin_url, image_url, update_time, parsing_time, price, total_area, living_area,
-            kitchen_area, room_number, floor, total_floor, housing, complex, point, state, city,
-            district, street, house_number, ssf, izf, gzf
+         	url, update_time, parsing_time, price, total_area, living_area, kitchen_area,
+            room_number, floor, total_floor, housing, complex, point, state, city, district,
+            street, house_number, ssf, izf, gzf
         )
         values 
 		(
-		    $1, $2, $3, now() at time zone 'utc', $4, $5, $6, $7, $8, $9, $10, $11, $12, 
-		    st_geomfromwkb($13, $14), $15, $16, $17, $18, $19, $20, $21, $22
+		    $1, $2, now() at time zone 'utc', $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, 
+		    st_geomfromwkb($13, $14), $15, $16, $17, $18, $19, $20, $21
 		)`,
 		flat.OriginURL,
-		flat.ImageURL,
 		flat.UpdateTime,
 		flat.Price,
 		flat.TotalArea,
