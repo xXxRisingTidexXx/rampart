606a62dddb8bfc41afa47fae13ff101e20fda2bc
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-13 14:58:34 +0300

    Seems to fix storing issue

--- internal/mining/domria/storer.go
+++ internal/mining/domria/storer.go
@@ -161,8 +161,8 @@ func (storer *Storer) createFlat(tx *sql.Tx, flat Flat) error {
         )
         values 
 		(
-		    $1, $2, now() at time zone 'utc', $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, 
-		    st_geomfromwkb($13, $14), $15, $16, $17, $18, $19, $20, $21
+		    $1, $2, now() at time zone 'utc', $3, $4, $5, $6, $7, $8, $9, $10, $11, 
+			st_geomfromwkb($12, $13), $14, $15, $16, $17, $18, $19, $20, $21
 		)`,
 		flat.URL,
 		flat.UpdateTime,
