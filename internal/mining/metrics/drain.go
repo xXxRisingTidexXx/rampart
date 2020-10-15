b4b86263f38532bffbbd24738b7364a3d548ab89
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-15 23:17:37 +0300

    Added sold validation column

--- internal/mining/metrics/drain.go
+++ internal/mining/metrics/drain.go
@@ -54,10 +54,12 @@ func (drain *Drain) Flush() {
     	    district_sanitation_number, swap_sanitation_number, street_sanitation_number,
     	    house_number_sanitation_number, located_geocoding_number, unlocated_geocoding_number,
     	    failed_geocoding_number, inconclusive_geocoding_number, successful_geocoding_number,
-    	    subwayless_ssf_gauging_number, failed_ssf_gauging_number, inconclusive_ssf_gauging_number,
-    	    successful_ssf_gauging_number, failed_izf_gauging_number, inconclusive_izf_gauging_number,
-    	    successful_izf_gauging_number, failed_gzf_gauging_number, inconclusive_gzf_gauging_number,
-    	    successful_gzf_gauging_number, approved_validation_number, uninformative_validation_number,
+    	    subwayless_ssf_gauging_number, failed_ssf_gauging_number,
+    	    inconclusive_ssf_gauging_number, successful_ssf_gauging_number,
+    	    failed_izf_gauging_number, inconclusive_izf_gauging_number,
+    	    successful_izf_gauging_number, failed_gzf_gauging_number,
+    	    inconclusive_gzf_gauging_number, successful_gzf_gauging_number,
+    	    approved_validation_number, uninformative_validation_number, sold_validation_number,
     	    denied_validation_number, created_storing_number, updated_storing_number,
     	    unaltered_storing_number, failed_storing_number, fetching_duration, geocoding_duration,
     	    ssf_gauging_duration, izf_gauging_duration, gzf_gauging_duration, reading_duration,
@@ -67,7 +69,7 @@ func (drain *Drain) Flush() {
     	(
     		now() at time zone 'utc', $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
     	    $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31,
-    	    $32, $33, $34, $35, $36, $37, $38
+    	    $32, $33, $34, $35, $36, $37, $38, $39
     	)`,
 		drain.miner,
 		drain.numbers[StateSanitationNumber],
@@ -93,6 +95,7 @@ func (drain *Drain) Flush() {
 		drain.numbers[SuccessfulGZFGaugingNumber],
 		drain.numbers[ApprovedValidationNumber],
 		drain.numbers[UninformativeValidationNumber],
+		drain.numbers[SoldValidationNumber],
 		drain.numbers[DeniedValidationNumber],
 		drain.numbers[CreatedStoringNumber],
 		drain.numbers[UpdatedStoringNumber],
