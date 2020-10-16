53ede0d21d1009bb38cc9a364e577cd75afc92e8
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-17 01:43:04 +0300

    Added image migration

---
+++ migrations/20201016223044_create_table_images.up.sql
@@ -0,0 +1,8 @@
+create table if not exists images
+(
+    id      serial primary key  not null,
+    flat_id int                 not null,
+    url     varchar(256) unique not null check ( url != '' ),
+    kind    varchar(10)         not null check ( kind != '' ),
+    constraint fk_images_flats foreign key (flat_id) references flats (id) on delete cascade
+);
