e3a6cd75e090903eac2fb6d84bf472c8ffa66f11
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-24 15:00:58 +0300

    Added todo

--- cmd/mining/main.go
+++ cmd/mining/main.go
@@ -16,6 +16,7 @@ import (
 // TODO: relative city center distance feature (with city diameter).
 // TODO: distance to workplace feature.
 // TODO: shorten house number column (but research the actual width before).
+// TODO: add envs to config.
 func main() {
 	isDebug := flag.Bool("debug", false, "Execute a single workflow instead of the whole schedule")
 	alias := flag.String("miner", "", "Desired miner alias")
