b8bdbbd265dc7f8cfa17d6531c3bc93623e08e0f
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-24 15:34:56 +0300

    Added help template

--- rampart/telebot/serve.py
+++ rampart/telebot/serve.py
@@ -8,7 +8,7 @@ def serve():
     updater = Updater(getenv('RAMPART_TELEBOT_TOKEN'))
     updater.dispatcher.add_handler(CommandHandler('start', server.get_start))
     # updater.dispatcher.add_handler(CommandHandler('search', _get_search))
-    # updater.dispatcher.add_handler(CommandHandler('help', _get_help))
+    updater.dispatcher.add_handler(CommandHandler('help', server.get_help))
     updater.start_polling()
     updater.idle()
 
@@ -17,6 +17,11 @@ class Server:
     def __init__(self):
         with open('templates/start.html') as stream:
             self._start_template = stream.read()
+        with open('templates/help.html') as stream:
+            self._help_template = stream.read()
 
     def get_start(self, update: Update, _: CallbackContext):
         update.message.reply_html(self._start_template)
+
+    def get_help(self, update: Update, _: CallbackContext):
+        update.message.reply_html(self._help_template)
