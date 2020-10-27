8691135f6275704f926ea736af6e2000d6836600
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-27 12:45:19 +0200

    Added input validation

--- rampart/browsing.py
+++ rampart/browsing.py
@@ -1,4 +1,6 @@
+from typing import Tuple, Union, Optional
 from flask import Flask, request, render_template
+from jinja2 import Template
 from rampart.config import BrowsingHandlerConfig, get_config
 from rampart.search import Query, Searcher
 
@@ -10,18 +12,51 @@ class Handler:
         self._index_name = config.index_name
         self._searcher = Searcher(config.searcher)
 
-    def get_index(self):
+    def get_index(self) -> Tuple[Union[str, Template], int]:
+        city = request.args.get('city')
+        if not city:
+            city = 'Київ'
+        price = _float(request.args.get('price'))
+        if price < 0:
+            return 'Invalid price provided', 400
+        put validationfloor = _int(request.args.get('floor'))
+        if floor < 0 or floor > 2:
+            return 'Invalid floor provided', 400
+        room_number = _int(request.args.get('room_number'))
+        if room_number < 0 or room_number > 4:
+            return 'Invalid floor provided', 400
+        limit = _int(request.args.get('limit'))
+        if limit < 1:
+            return 'Invalid limit provided', 400
+        offset = _int(request.args.get('offset'))
+        if offset < 0:
+            return 'Invalid offset provided', 400
         print(
-            Query(
-                request.args.get('city'),
-                request.args.get('price'),
-                request.args.get('floor'),
-                request.args.get('room_number'),
-                request.args.get('limit'),
-                request.args.get('offset')
+            len(
+                self._searcher.search_flats(
+                    Query(city, price, floor, room_number, limit, offset)
+                )
             )
         )
-        return render_template(self._index_name)
+        return render_template(self._index_name), 200
+
+
+def _float(value: Optional[str]) -> float:
+    if not value:
+        return 0
+    try:
+        return float(value)
+    except ValueError:
+        return -1
+
+
+def _int(value: Optional[str]) -> int:
+    if not value:
+        return 0
+    try:
+        return int(value)
+    except ValueError:
+        return -1
 
 
 if __name__ == '__main__':
