1ad5bed90a79c7482c156790ceea40c159b9fc10
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-10-31 15:30:32 +0200

    Formatted code

--- internal/domria/number.go
+++ internal/domria/number.go
@@ -7,7 +7,7 @@ func (n *number) UnmarshalJSON(bytes []byte) error {
 	if s[0] == '"' {
 		s = s[1:]
 	}
-	if i := len(s)-1; s[i] == '"' {
+	if i := len(s) - 1; s[i] == '"' {
 		s = s[:i]
 	}
 	*n = number(s)
