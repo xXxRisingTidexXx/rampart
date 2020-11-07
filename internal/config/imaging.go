e741ac1dbdd5886368eabab5ba23777baee1d3e2
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-11-07 17:14:04 +0200

    Added request timeout

--- internal/config/imaging.go
+++ internal/config/imaging.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
+	"time"
 )
 
 type Imaging struct {
-	ThreadNumber int          `yaml:"thread-number"`
-	Headers      misc.Headers `yaml:"headers"`
-	OutputFormat string       `yaml:"output-format"`
-	InputPath    string       `yaml:"input-path"`
+	Timeout      time.Duration `yaml:"timeout"`
+	ThreadNumber int           `yaml:"thread-number"`
+	Headers      misc.Headers  `yaml:"headers"`
+	OutputFormat string        `yaml:"output-format"`
+	InputPath    string        `yaml:"input-path"`
 }
