ce8bb90be2f99931fae36b82e462f5d3c916c097
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-11-14 21:45:48 +0200

    Added retry config

--- internal/config/imaging.go
+++ internal/config/imaging.go
@@ -8,6 +8,7 @@ import (
 type Imaging struct {
 	Timeout      time.Duration `yaml:"timeout"`
 	ThreadNumber int           `yaml:"thread-number"`
+	RetryLimit   int           `yaml:"retry-limit"`
 	Headers      misc.Headers  `yaml:"headers"`
 	OutputFormat string        `yaml:"output-format"`
 	InputPath    string        `yaml:"input-path"`
