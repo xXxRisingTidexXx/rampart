0512c176af09cc8fd2676031f3560230d8db8c04
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-04 14:22:32 +0200

    Almost finished augmentation

--- cmd/augmentation/main.go
+++ cmd/augmentation/main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/disintegration/gift"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
-	_ "golang.org/x/image/webp"
 	"image"
 	"os"
 )
@@ -13,17 +12,26 @@ func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
 	entry := log.WithField("app", "augmentation")
-	file, err := os.Open(misc.ResolvePath("images/demo.webp"))
+	file, err := os.Open(misc.ResolvePath("images/source.webp"))
 	if err != nil {
-		entry.Fatalf("main: augmentation failed to open the file, %v", err)
+		entry.Fatalf("main: augmentation failed to open the source, %v", err)
 	}
-	_, _, err = image.Decode(file)
+	source, _, err := image.Decode(file)
 	if err != nil {
 		_ = file.Close()
-		entry.Fatalf("main: augmentation failed to decode the image, %v", err)
+		entry.Fatalf("main: augmentation failed to decode the source, %v", err)
 	}
-	_ = gift.New()  // TODO
 	if err := file.Close(); err != nil {
-		entry.Fatalf("main: augmentation failed to close the file, %v", err)
+		entry.Fatalf("main: augmentation failed to close the source, %v", err)
+	}
+	g := gift.New(gift.Transpose())
+	target := image.NewRGBA(g.Bounds(source.Bounds()))
+	g.Draw(target, source)
+	file, err = os.Create("images/target.webp")
+	if err != nil {
+		entry.Fatalf("main: augmentation failed to create the target, %v", err)
+	}
+	if err := file.Close(); err != nil {
+		entry.Fatalf("main: augmentation failed to close the target, %v", err)
 	}
 }
