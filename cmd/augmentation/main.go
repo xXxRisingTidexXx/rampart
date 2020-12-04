3ec3b7eb5f523a0532d3b76cfd316e27440da805
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-04 13:49:03 +0200

    Added image decoding

--- cmd/augmentation/main.go
+++ cmd/augmentation/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"github.com/disintegration/gift"
-	"runtime"
+	log "github.com/sirupsen/logrus"
+	"github.com/xXxRisingTidexXx/rampart/internal/misc"
+	"image"
+	"os"
 )
 
 func main() {
-	_ = gift.New()
-	fmt.Println(runtime.NumCPU())
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetReportCaller(true)
+	entry := log.WithField("app", "augmentation")
+	file, err := os.Open(misc.ResolvePath("images/demo.webp"))
+	if err != nil {
+		entry.Fatalf("main: augmentation failed to open the file, %v", err)
+	}
+	_, _, err = image.Decode(file)
+	if err != nil {
+		_ = file.Close()
+		entry.Fatalf("main: augmentation failed to decode the image, %v", err)
+	}
+	if err := file.Close(); err != nil {
+		entry.Fatalf("main: augmentation failed to close the file, %v", err)
+	}
 }
