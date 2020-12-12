cc60bf15e0444946a4f7c40ea63c85496ca73fc2
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-12 08:56:56 +0200

    Made file reading atomic

--- cmd/augmentation/main.go
+++ cmd/augmentation/main.go
@@ -1,33 +1,29 @@
 package main
 
 import (
+	gobytes "bytes"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/imaging"
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
 	"image"
 	"io/ioutil"
-	"os"
 )
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
 	entry := log.WithField("app", "augmentation")
-	file, err := os.Open(misc.ResolvePath("images/source.webp"))
+	bytes, err := ioutil.ReadFile(misc.ResolvePath("images/source.webp"))
 	if err != nil {
-		entry.Fatalf("main: augmentation failed to open the source, %v", err)
+		entry.Fatalf("main: augmentation failed to read the source, %v", err)
 	}
-	source, _, err := image.Decode(file)
+	source, _, err := image.Decode(gobytes.NewBuffer(bytes))
 	if err != nil {
-		_ = file.Close()
 		entry.Fatalf("main: augmentation failed to decode the source, %v", err)
-	}
-	if err := file.Close(); err != nil {
-		entry.Fatalf("main: augmentation failed to close the source, %v", err)
 	}
 	for _, effect := range imaging.Effects {
 		entry = entry.WithField("effect", effect.Name())
-		bytes, err := effect.Apply(source)
+		bytes, err = effect.Apply(source)
 		if err != nil {
 			entry.Fatal(err)
 		}
