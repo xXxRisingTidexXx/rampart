e46743f8b9ddbd078ac9606409628da5a003979c
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-16 02:20:03 +0200

    Removed source printing

--- internal/domria/storer.go
+++ internal/domria/storer.go
@@ -28,9 +28,9 @@ type Storer struct {
 
 func (storer *Storer) StoreFlats(flats []Flat) {
 	for _, flat := range flats {
-		entry := storer.logger.WithField("source", flat.Source)
+		entry := storer.logger.WithField("url", flat.URL)
 		if id, err := storer.storeFlat(flat); err != nil {
-			entry.WithField("url", flat.URL).Error(err)
+			entry.Error(err)
 			storer.drain.DrainNumber(metrics.FailedFlatStoringNumber)
 		} else {
 			storer.storeImages(id, flat.Photos, PhotoKind, entry)
