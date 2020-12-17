318ec633f9eb8ce82e8b59a4e60aa625012b9b48
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-17 14:56:57 +0200

    Simplified browsing & removed handler

--- rampart/browsing.py
+++ rampart/browsing.py
@@ -1,50 +1,51 @@
 from typing import Optional
 from flask import Flask, request, render_template, abort
-from rampart.config import BrowsingHandlerConfig, get_config
+from rampart.config import get_config
 from rampart.search import Query, Searcher
 
 
-class Handler:
-    __slots__ = ['_index_name', '_searcher']
+def _main():
+    config = get_config()
+    searcher = Searcher(config.browsing.searcher)
+    app = Flask('rampart.browsing', template_folder=config.browsing.template_path)
+    app.add_url_rule('/', view_func=lambda: _get_index(searcher), methods=['GET'])
+    app.run('0.0.0.0', config.browsing.port, load_dotenv=False, use_reloader=False)
 
-    def __init__(self, config: BrowsingHandlerConfig):
-        self._index_name = config.index_name
-        self._searcher = Searcher(config.searcher)
 
-    def get_index(self) -> str:
-        city = request.args.get('city')
-        if not city:
-            city = 'Київ'
-        price = _float(request.args.get('price'))
-        if price < 0:
-            abort(400)
-        floor = _int(request.args.get('floor'))
-        if floor < 0 or floor > 2:
-            abort(400)
-        room_number = _int(request.args.get('room_number'))
-        if room_number < 0 or room_number > 4:
-            abort(400)
-        limit = _int(request.args.get('limit'), 10)
-        if limit < 1:
-            abort(400)
-        offset = _int(request.args.get('offset'))
-        if offset < 0:
-            abort(400)
-        query = Query(city, price, floor, room_number, limit, offset)
-        return render_template(
-            self._index_name,
-            lower=query.lower + 1,
-            upper=query.upper,
-            city=city,
-            price=price,
-            floor=floor,
-            room_number=room_number,
-            limit=limit,
-            offset=offset,
-            previous=offset - 1,
-            next=offset + 1,
-            flats=self._searcher.search_flats(query)
-        )
+def _get_index(searcher: Searcher) -> str:
+    city = request.args.get('city')
+    if not city:
+        city = 'Київ'
+    price = _float(request.args.get('price'))
+    if price < 0:
+        abort(400)
+    floor = _int(request.args.get('floor'))
+    if floor < 0 or floor > 2:
+        abort(400)
+    room_number = _int(request.args.get('room_number'))
+    if room_number < 0 or room_number > 4:
+        abort(400)
+    limit = _int(request.args.get('limit'), 10)
+    if limit < 1:
+        abort(400)
+    offset = _int(request.args.get('offset'))
+    if offset < 0:
+        abort(400)
+    query = Query(city, price, floor, room_number, limit, offset)
+    return render_template(
+        'index.html',
+        lower=query.lower + 1,
+        upper=query.upper,
+        city=city,
+        price=price,
+        floor=floor,
+        room_number=room_number,
+        limit=limit,
+        offset=offset,
+        previous=offset - 1,
+        next=offset + 1,
+        flats=searcher.search_flats(query)
+    )
 
 
 def _float(value: Optional[str]) -> float:
@@ -66,8 +67,4 @@ def _int(value: Optional[str], default: int = 0) -> int:
 
 
 if __name__ == '__main__':
-    setup = get_config().browsing
-    app = Flask('rampart.browsing', template_folder=setup.template_path)
-    handler = Handler(setup.handler)
-    app.add_url_rule('/', view_func=handler.get_index, methods=['GET'])
-    app.run('0.0.0.0', setup.port, load_dotenv=False, use_reloader=False)
+    _main()
--- rampart/config.py
+++ rampart/config.py
@@ -1,12 +1,8 @@
 from os import getenv
-from re import compile
 from pathlib import Path
-from typing import Dict
 from yaml import safe_load
-from rampart.models import Housing
 
 _root_path = Path(__file__).parent.parent
-_template_path = _root_path / 'templates'
 
 
 def get_config() -> 'Config':
@@ -15,63 +11,19 @@ def get_config() -> 'Config':
 
 
 class Config:
-    __slots__ = ['telebot', 'browsing']
+    __slots__ = ['browsing']
 
     def __init__(self, config):
-        self.telebot = TelebotConfig(config['telebot'])
         self.browsing = BrowsingConfig(config['browsing'])
 
 
-class TelebotConfig:
-    __slots__ = ['token', 'pattern', 'handler']
+class BrowsingConfig:
+    __slots__ = ['port', 'template_path', 'searcher']
 
     def __init__(self, config):
-        self.token = _get_env('RAMPART_TELEBOT_TOKEN')
-        self.pattern = compile(config['pattern'])
-        self.handler = TelebotHandlerConfig(config['handler'])
-
-
-def _get_env(key: str) -> str:
-    value = getenv(key)
-    if not value:
-        raise RuntimeError(f'Environment variable \'{key}\' not set')
-    return value
-
-
-class TelebotHandlerConfig:
-    __slots__ = [
-        'any',
-        'cities',
-        'floors',
-        'room_numbers',
-        'housings',
-        'start_template',
-        'help_template',
-        'nothing_template',
-        'flat_template',
-        'confusion_template',
-        'searcher'
-    ]
-
-    def __init__(self, config):
-        self.any: str = config['any']
-        self.cities: Dict[str, str] = config['cities']
-        self.floors: Dict[str, int] = config['floors']
-        self.room_numbers: Dict[str, int] = config['room-numbers']
-        self.housings: Dict[Housing, str] = {
-            Housing(i): h for i, h in enumerate(config['housings'])
-        }
-        self.start_template = _read_template('start.html')
-        self.help_template = _read_template('help.html')
-        self.nothing_template = _read_template('nothing.html')
-        self.flat_template = _read_template('flat.html')
-        self.confusion_template = _read_template('confusion.html')
+        self.port: int = config['port']
+        self.template_path = _root_path / 'templates'
         self.searcher = SearcherConfig()
-
-
-def _read_template(name: str) -> str:
-    with open(_template_path / name) as stream:
-        return stream.read()
 
 
 class SearcherConfig:
@@ -79,21 +31,11 @@ class SearcherConfig:
 
     def __init__(self):
         self.dsn = _get_env('RAMPART_DATABASE_DSN')
-        self.model_path = str(_root_path / 'model.txt')
+        self.model_path = str(_root_path / 'scientific/models/twinkle.latest.txt')
 
 
-class BrowsingConfig:
-    __slots__ = ['port', 'template_path', 'handler']
-
-    def __init__(self, config):
-        self.port: int = config['port']
-        self.template_path = _template_path
-        self.handler = BrowsingHandlerConfig()
-
-
-class BrowsingHandlerConfig:
-    __slots__ = ['index_name', 'searcher']
-
-    def __init__(self):
-        self.index_name = 'index.html'
-        self.searcher = SearcherConfig()
+def _get_env(key: str) -> str:
+    value = getenv(key)
+    if not value:
+        raise RuntimeError(f'Environment variable \'{key}\' not set')
+    return value
