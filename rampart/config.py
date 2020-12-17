ea82b441aebe42af218147bdba6fb3d8ff04902c
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-18 01:13:20 +0200

    Added auge section

--- rampart/config.py
+++ rampart/config.py
@@ -1,5 +1,6 @@
 from os import getenv
 from pathlib import Path
+from typing import Any, Dict
 from yaml import safe_load
 
 _root_path = Path(__file__).parent.parent
@@ -11,27 +12,12 @@ def get_config() -> 'Config':
 
 
 class Config:
-    __slots__ = ['browsing']
+    __slots__ = ['browsing', 'auge']
 
-    def __init__(self, config):
-        self.browsing = BrowsingConfig(config['browsing'])
-
-
-class BrowsingConfig:
-    __slots__ = ['port', 'template_path', 'ranker']
-
-    def __init__(self, config):
-        self.port: int = config['port']
-        self.template_path = _root_path / 'templates'
-        self.ranker = RankerConfig()
-
-
-class RankerConfig:
-    __slots__ = ['dsn', 'model_path']
-
-    def __init__(self):
-        self.dsn = _get_env('RAMPART_DATABASE_DSN')
-        self.model_path = str(_root_path / 'scientific/models/twinkle.latest.txt')
+    def __init__(self, config: Dict[str, Any]):
+        dsn = _get_env('RAMPART_DATABASE_DSN')
+        self.browsing = BrowsingConfig(config['browsing'], dsn)
+        self.auge = AugeConfig(config['auge'], dsn)
 
 
 def _get_env(key: str) -> str:
@@ -39,3 +25,30 @@ def _get_env(key: str) -> str:
     if not value:
         raise RuntimeError(f'Environment variable \'{key}\' not set')
     return value
+
+
+class BrowsingConfig:
+    __slots__ = ['port', 'template_path', 'ranker']
+
+    def __init__(self, config: Dict[str, Any], dsn: str):
+        self.port: int = config['port']
+        self.template_path = _root_path / 'templates'
+        self.ranker = RankerConfig(dsn)
+
+
+class RankerConfig:
+    __slots__ = ['dsn', 'model_path']
+
+    def __init__(self, dsn: str):
+        self.dsn = dsn
+        self.model_path = str(_root_path / 'scientific/models/twinkle.latest.txt')
+
+
+class AugeConfig:
+    __slots__ = ['thread_number', 'retry_limit', 'dsn', 'model_path']
+
+    def __init__(self, config: Dict[str, Any], dsn: str):
+        self.thread_number: int = config['thread-number']
+        self.retry_limit: int = config['retry-limit']
+        self.dsn = dsn
+        self.model_path = str(_root_path / 'scientific/models/auge.latest.pth')
