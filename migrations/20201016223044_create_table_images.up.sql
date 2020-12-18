759cb3b6e75485fe09f3808ec9e9d867abebfbc6
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-18 13:40:04 +0200

    Added label column

--- migrations/20201016223044_create_table_images.up.sql
+++ migrations/20201016223044_create_table_images.up.sql
@@ -3,7 +3,8 @@ create table if not exists images
     id           serial primary key  not null,
     flat_id      int                 not null,
     url          varchar(256) unique not null check ( url != '' ),
-    kind         varchar(10)         not null check ( kind != '' ),
     parsing_time timestamp           not null,
+    kind         varchar(10)         not null check ( kind != '' ),
+    label        varchar(15)         not null check ( label != '' ) default 'unknown',
     constraint fk_images_flats foreign key (flat_id) references flats (id) on delete cascade
 );
