70df26c1f5edea4bd0c01080837c887ce79d1af3
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-30 19:38:01 +0200

    Added serialization & augmentation logic

--- rampart/coquus.py
+++ rampart/coquus.py
@@ -1,6 +1,10 @@
 from sys import exit
 from uuid import uuid4
-from pandas import read_csv, DataFrame
+from pandas import read_csv, DataFrame, concat
+from numpy import ndarray
+from numpy.random import rand
+from lightgbm import Dataset
+from rampart.config import get_config
 from rampart.logging import get_logger
 
 _logger = get_logger('rampart.coquus')
@@ -8,7 +12,8 @@ _logger = get_logger('rampart.coquus')
 
 # TODO: setup basic logger with lineno & timestamp.
 def _main():
-    flats = read_csv('scientific/twinkle.csv').drop(columns=['url'])
+    config = get_config()
+    flats = read_csv(config.coquus.input_path).drop(columns=['url'])
     group_number = flats['group'].nunique()
     groups = ['training', 'validation', 'testing']
     if group_number != len(groups):
@@ -19,11 +24,41 @@ def _main():
         exit(1)
     tag = uuid4().hex
     for i, group in enumerate(groups):
-        _augment(flats[flats['group'] == i].drop(columns=['group']), tag, group)
+        origin = flats[flats['group'] == i].drop(columns=['group'])
+        merge = concat(
+            [origin] + [_augment(origin, i) for i in range(1, 32)],
+            ignore_index=True
+        )
+        dataset = Dataset(
+            merge.drop(columns=['relevance', 'query']),
+            merge['relevance'],
+            group=merge.groupby(['query']).size(),
+            categorical_feature=[
+                'desired_room_number',
+                'desired_floor',
+                'housing'
+            ],
+            silent=True
+        )
+        dataset.save_binary(config.coquus.output_format.format(tag, group))
+        dataset.save_binary(config.coquus.output_format.format('latest', group))
 
 
-def _augment(flats: DataFrame, tag: str, group: str):
-    print(flats['query'].unique())
+def _augment(origin: DataFrame, i: int):
+    copy = origin.copy()
+    copy['actual_price'] *= _shake(0.001, len(origin))
+    copy['total_area'] *= _shake(0.005, len(origin))
+    copy['living_area'] *= _shake(0.01, len(origin))
+    copy['kitchen_area'] *= _shake(0.01, len(origin))
+    copy['ssf'] *= _shake(0.007, len(origin))
+    copy['izf'] *= _shake(0.007, len(origin))
+    copy['gzf'] *= _shake(0.007, len(origin))
+    copy['query'] += i * origin['query'].nunique()
+    return copy
+
+
+def _shake(deviation: float, size: int) -> ndarray:
+    return 1 - deviation + 2 * deviation * rand(size)
 
 
 if __name__ == '__main__':
