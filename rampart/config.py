81200bb3450d2da2c80cf640d7eaee8a80c8ad44
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-12-31 00:12:49 +0200

    Unified section creation

--- rampart/config.py
+++ rampart/config.py
@@ -12,13 +12,15 @@ def get_config() -> 'Config':
 
 
 class Config:
-    __slots__ = ['browsing', 'coquus', 'auge']
+    __slots__ = ['coquus', 'hemingway', 'auge']
 
     def __init__(self, config: Dict[str, Any]):
         dsn = _get_env('RAMPART_DATABASE_DSN')
-        self.browsing = BrowsingConfig(config['browsing'], dsn)
+        config['hemingway']['dsn'] = dsn
+        config['auge']['dsn'] = dsn
         self.coquus = CoquusConfig(config['coquus'])
-        self.auge = AugeConfig(config['auge'], dsn)
+        self.hemingway = HemingwayConfig(config['hemingway'])
+        self.auge = AugeConfig(config['auge'])
 
 
 def _get_env(key: str) -> str:
@@ -28,32 +30,29 @@ def _get_env(key: str) -> str:
     return value
 
 
-class BrowsingConfig:
-    __slots__ = ['port', 'template_path', 'ranker']
-
-    def __init__(self, config: Dict[str, Any], dsn: str):
-        self.port: int = config['port']
-        self.template_path = _root_path / 'templates'
-        self.ranker = RankerConfig(dsn)
-
-
-# TODO: move model path to config.
-class RankerConfig:
-    __slots__ = ['dsn', 'model_path']
-
-    def __init__(self, dsn: str):
-        self.dsn = dsn
-        self.model_path = str(
-            _root_path / 'scientific/models/twinkle.latest.txt'
-        )
-
-
 class CoquusConfig:
     __slots__ = ['input_path', 'output_format']
 
     def __init__(self, config: Dict[str, Any]):
         self.input_path = str(_root_path / config['input-path'])
         self.output_format = str(_root_path / config['output-format'])
+
+
+class HemingwayConfig:
+    __slots__ = ['dsn', 'port', 'template_path', 'ranker']
+
+    def __init__(self, config: Dict[str, Any]):
+        self.dsn: str = config['dsn']
+        self.port: int = config['port']
+        self.template_path = str(_root_path / 'templates')
+        self.ranker = RankerConfig(config['ranker'])
+
+
+class RankerConfig:
+    __slots__ = ['model_path']
+
+    def __init__(self, config: Dict[str, Any]):
+        self.model_path = str(_root_path / config['model-path'])
 
 
 class AugeConfig:
@@ -66,8 +65,8 @@ class AugeConfig:
         'spec'
     ]
 
-    def __init__(self, config: Dict[str, Any], dsn: str):
-        self.dsn = dsn
+    def __init__(self, config: Dict[str, Any]):
+        self.dsn: str = config['dsn']
         self.pool_size: int = config['pool-size']
         self.retry_limit: int = config['retry-limit']
         self.recognizer = RecognizerConfig(config['recognizer'])
