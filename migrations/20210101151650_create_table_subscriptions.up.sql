b0f4adaa6718d9f0c121bf2f4e67fd71670d1daf
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-01 23:38:25 +0200

    Allowed arbitrary price

--- migrations/20210101151650_create_table_subscriptions.up.sql
+++ migrations/20210101151650_create_table_subscriptions.up.sql
@@ -3,7 +3,7 @@ create table if not exists subscriptions
     id          serial primary key not null,
     chat_id     bigint             not null,
     city        varchar(50)        not null check ( city != '' ),
-    price       real               not null check ( 0 < price ),
+    price       real               not null check ( 0 <= price ),
     room_number varchar(5)         not null check ( room_number != '' ),
     floor       varchar(5)         not null check ( floor != '' )
 );
