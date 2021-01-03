93ca496eb1c390391d703ae58daf6bd14a326a86
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-04 00:50:18 +0200

    Merged entries into lookups

--- migrations/20201016223044_create_table_images.up.sql
+++ migrations/20201016223044_create_table_images.up.sql
@@ -1,7 +1,7 @@
 create table if not exists images
 (
     id      serial primary key not null,
-    flat_id int                not null,
+    flat_id integer            not null,
     url     varchar(256)       not null check ( url != '' ),
     kind    varchar(10)        not null check ( kind != '' ),
     label   varchar(15)        not null check ( label != '' ) default 'unknown',
