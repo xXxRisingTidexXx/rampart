fc38ad7c902ebae2dcafd89a3e9ec0edc4844076
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-04 19:35:18 +0200

    Added primitive responses

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -7,9 +7,12 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 	"github.com/xXxRisingTidexXx/rampart/internal/metrics"
+	"time"
 )
 
 // TODO: think about signal handling & graceful shutdown.
+// TODO: think about viber integration, https://github.com/mileusna/viber .
+// TODO: configure bot commands at BotFather.
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
@@ -34,9 +37,28 @@ func main() {
 		_ = db.Close()
 		entry.Fatalf("main: telegram failed to instantiate, %v", err)
 	}
+	updates, _ := bot.GetUpdatesChan(tgbotapi.UpdateConfig{Timeout: c.Telegram.Timeout})
+	time.Sleep(200 * time.Millisecond)
+	updates.Clear()
 	metrics.RunServer(c.Telegram.Server, entry)
-	bot.Debug = true
-
+	for update := range updates {
+		if update.Message != nil && update.Message.Chat != nil && update.Message.IsCommand() {
+			command, text := update.Message.Command(), "Hello from default!"
+			switch command {
+			case "start":
+				text = "Hello from start!"
+			case "help":
+				text = "Hello from help!"
+			default:
+				command = "default"
+			}
+			metrics.TelegramCommands.WithLabelValues(command).Inc()
+			_, err := bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, text))
+			if err != nil {
+				entry.Errorf("main: telegram failed to send a message, %v", err)
+			}
+		}
+	}
 	bot.StopReceivingUpdates()
 	if err = db.Close(); err != nil {
 		entry.Fatalf("main: telegram failed to close the db, %v", err)
