905b5cbf2fa30fa7683e4c4ed116562fdb7875a5
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-04 17:27:31 +0200

    Added required imports & preparations

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 	"github.com/xXxRisingTidexXx/rampart/internal/metrics"
@@ -12,6 +13,9 @@ func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
 	entry := log.WithField("app", "telegram")
+	if err := tgbotapi.SetLogger(entry); err != nil {
+		entry.Fatalf("main: telegram failed to set the logger, %v", err)
+	}
 	c, err := config.NewConfig()
 	if err != nil {
 		entry.Fatal(err)
@@ -24,13 +28,15 @@ func main() {
 		_ = db.Close()
 		entry.Fatalf("main: telegram failed to ping the db, %v", err)
 	}
-	_, err = tgbotapi.NewBotAPI(c.Telegram.Token)
+	bot, err := tgbotapi.NewBotAPI(c.Telegram.Token)
 	if err != nil {
 		_ = db.Close()
 		entry.Fatalf("main: telegram failed to instantiate, %v", err)
 	}
 	metrics.RunServer(c.Telegram.Server, entry)
+	bot.Debug = true
 
+	bot.StopReceivingUpdates()
 	if err = db.Close(); err != nil {
 		entry.Fatalf("main: telegram failed to close the db, %v", err)
 	}
