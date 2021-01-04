47f23fffdb75f7da94d4bda00ca53957f1beea83
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-05 01:42:15 +0200

    Added template format

--- internal/config/dispatcher.go
+++ internal/config/dispatcher.go
@@ -1,7 +1,8 @@
 package config
 
 type Dispatcher struct {
-	Token        string `yaml:"-"`
-	Timeout      int    `yaml:"timeout"`
-	WorkerNumber int    `yaml:"worker-number"`
+	Token          string `yaml:"-"`
+	Timeout        int    `yaml:"timeout"`
+	WorkerNumber   int    `yaml:"worker-number"`
+	TemplateFormat string `yaml:"template-format"`
 }
