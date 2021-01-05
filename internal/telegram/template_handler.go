281a2f195ef38a2c97ddbb32356423da420ebc39
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-05 02:17:00 +0200

    Made handler over

--- internal/telegram/template_handler.go
+++ internal/telegram/template_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
+	"io/ioutil"
 )
 
 func NewHTMLHandler(command, format string) Handler {
@@ -17,12 +18,22 @@ type htmlHandler struct {
 }
 
 func (handler *htmlHandler) ShouldServe(update tgbotapi.Update) bool {
-	panic("implement me")
+	return update.Message != nil &&
+		update.Message.Chat != nil &&
+		update.Message.Command() == handler.command
 }
 
 func (handler *htmlHandler) ServeUpdate(
 	bot *tgbotapi.BotAPI,
 	update tgbotapi.Update,
 ) (log.Fields, error) {
-	panic("implement me")
+	fields := log.Fields{"handler": "template", "command": handler.command}
+	bytes, err := ioutil.ReadFile(handler.path)
+	if err != nil {
+		return fields, fmt.Errorf("telegram: html handler failed to read the path, %v", err)
+	}
+	message := tgbotapi.NewMessage(update.Message.Chat.ID, string(bytes))
+	message.ParseMode = tgbotapi.ModeHTML
+
+	return nil, nil
 }
