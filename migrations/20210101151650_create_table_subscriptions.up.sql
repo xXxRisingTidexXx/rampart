996ad42e3f7a3c63a2634054da19023aa33937fe
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-05 11:49:39 +0200

    Added creation time && status

--- migrations/20210101151650_create_table_subscriptions.up.sql
+++ migrations/20210101151650_create_table_subscriptions.up.sql
@@ -1,9 +1,11 @@
 create table if not exists subscriptions
 (
-    id          serial primary key not null,
-    chat_id     bigint             not null,
-    city        varchar(50)        not null check ( city != '' ),
-    price       real               not null check ( 0 <= price ),
-    room_number varchar(5)         not null check ( room_number != '' ),
-    floor       varchar(5)         not null check ( floor != '' )
+    id            serial primary key not null,
+    chat_id       bigint             not null,
+    creation_time timestamp          not null,
+    status        varchar(10)        not null check ( status != '' ),
+    city          varchar(50)        not null check ( city != '' ),
+    price         real               not null check ( 0 <= price ),
+    room_number   varchar(5)         not null check ( room_number != '' ),
+    floor         varchar(5)         not null check ( floor != '' )
 );
