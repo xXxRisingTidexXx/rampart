1753485b163c9613e60b660a0c0a51ab5d919e84
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-10 15:50:08 +0200

    Extended status length

--- migrations/20210109194123_create_table_transients.up.sql
+++ migrations/20210109194123_create_table_transients.up.sql
@@ -1,7 +1,7 @@
 create table if not exists transients
 (
     id          bigint primary key not null,
-    status      varchar(10)        not null check ( status != '' ),
+    status      varchar(15)        not null check ( status != '' ),
     city        varchar(50)        not null check ( city != '' ),
     price       real               not null check ( 0 <= price ),
     room_number varchar(10)        not null check ( room_number != '' ),
