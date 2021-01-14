df1c69da280d3afc24777bedb63dc89e5c67d2a6
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-15 01:04:13 +0200

    Added full dialog handler flow

--- internal/telegram/dialog_handler.go
+++ internal/telegram/dialog_handler.go
@@ -2,18 +2,60 @@ package telegram
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
 )
 
-func NewDialogHandler(bot *tgbotapi.BotAPI, db * sql.DB) Handler {
-	return &dialogHandler{&helper{bot}}
+func NewDialogHandler(bot *tgbotapi.BotAPI, db *sql.DB) Handler {
+	return &dialogHandler{bot, db, map[Status]StatusHandler{}}
 }
 
 type dialogHandler struct {
-	helper *helper
+	bot      *tgbotapi.BotAPI
+	db       *sql.DB
+	handlers map[Status]StatusHandler
 }
 
 func (h *dialogHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
-	return log.Fields{}, nil
+	fields := log.Fields{"handler": "dialog"}
+	tx, err := h.db.Begin()
+	if err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to begin a transaction, %v", err)
+	}
+	var view string
+	row := tx.QueryRow(`select status from transients where id = $1`, update.Message.Chat.ID)
+	if err := row.Scan(&view); err == sql.ErrNoRows {
+		if err := tx.Commit(); err != nil {
+			return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
+		}
+		return fields, nil
+	} else if err != nil {
+		_ = tx.Rollback()
+		return fields, fmt.Errorf("telegram: handler failed to read a transient, %v", err)
+	}
+	status, err := ToStatus(view)
+	if err != nil {
+		_ = tx.Rollback()
+		return fields, err
+	}
+	handler, ok := h.handlers[status]
+	if !ok {
+		_ = tx.Rollback()
+		fields["status"] = view
+		return fields, fmt.Errorf("telegram: handler failed to handle a status")
+	}
+	fields = log.Fields{"handler": view}
+	message, err := handler.HandleStatusUpdate(update, tx)
+	if err != nil {
+		_ = tx.Rollback()
+		return fields, err
+	}
+	if err := tx.Commit(); err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
+	}
+	if _, err := h.bot.Send(message); err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to send a message, %v", err)
+	}
+	return fields, nil
 }
