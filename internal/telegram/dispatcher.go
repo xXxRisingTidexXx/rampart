f017d314a560cc2dc86ecbda8429270e86644bbc
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-14 13:40:19 +0200

    Injected custom field facade

--- internal/telegram/dispatcher.go
+++ internal/telegram/dispatcher.go
@@ -6,10 +6,10 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
+	"github.com/xXxRisingTidexXx/rampart/internal/metrics"
 	"sync"
 )
 
-// TODO: instead of separate state handlers use conversation handler supplying transaction.
 func NewDispatcher(
 	config config.Dispatcher,
 	db *sql.DB,
@@ -23,15 +23,7 @@ func NewDispatcher(
 		bot,
 		config.Timeout,
 		config.WorkerNumber,
-		[]XHandler{
-			NewStartHandler(db),
-			NewHelpHandler(),
-			NewAddHandler(db),
-			NewCityHandler(db),
-			NewPriceHandler(db),
-			NewRoomNumberHandler(db),
-			NewFloorHandler(db),
-		},
+		NewRootHandler(bot, db),
 		logger,
 	}, nil
 }
@@ -40,7 +32,7 @@ type Dispatcher struct {
 	bot          *tgbotapi.BotAPI
 	timeout      int
 	workerNumber int
-	handlers     []XHandler
+	handler      Handler
 	logger       log.FieldLogger
 }
 
@@ -56,20 +48,11 @@ func (dispatcher *Dispatcher) Dispatch() {
 
 func (dispatcher *Dispatcher) work(updates tgbotapi.UpdatesChannel, group *sync.WaitGroup) {
 	for update := range updates {
-		if update.Message != nil {
-			dispatcher.logger.Info(update.Message.Text)
+		fields, err := dispatcher.handler.HandleUpdate(update)
+		if err != nil {
+			dispatcher.logger.WithFields(fields).Error(err)
 		}
-		//for i := 0; i < len(dispatcher.handlers) && !ok; i++ {
-		//
-		//	ok, err = dispatcher.handlers[i].XHandleUpdate(dispatcher.bot, update)
-		//	if ok {
-		//		handler = dispatcher.handlers[i].Name()
-		//	}
-		//	if err != nil {
-		//		dispatcher.logger.WithField("handler", dispatcher.handlers[i].Name()).Error(err)
-		//	}
-		//}
-		//metrics.TelegramUpdates.WithLabelValues(handler).Inc()
+		metrics.TelegramUpdates.WithLabelValues(fields["handler"].(string)).Inc()
 	}
 	group.Done()
 }
--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -2,9 +2,8 @@ package telegram
 
 import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
-	log "github.com/sirupsen/logrus"
 )
 
 type Handler interface {
-	HandleUpdate(update tgbotapi.Update) (log.Fields, error)
+	HandleUpdate(update tgbotapi.Update) (Fields, error)
 }
