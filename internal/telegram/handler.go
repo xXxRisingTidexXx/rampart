1b3ee094ef1d85dc15f6d1d83513a54d9292e9ec
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-14 13:49:07 +0200

    Removed custom fields

--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -2,8 +2,9 @@ package telegram
 
 import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	log "github.com/sirupsen/logrus"
 )
 
 type Handler interface {
-	HandleUpdate(update tgbotapi.Update) (Fields, error)
+	HandleUpdate(update tgbotapi.Update) (log.Fields, error)
 }
