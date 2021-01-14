bc1a71a1b168dd6f217e0f069dc8144a64e5d43f
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-14 13:49:18 +0200

    Added root handler

---
+++ internal/telegram/root_handler.go
@@ -0,0 +1,22 @@
+package telegram
+
+import (
+	"database/sql"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	log "github.com/sirupsen/logrus"
+)
+
+func NewRootHandler(bot *tgbotapi.BotAPI, db *sql.DB) Handler {
+	return &rootHandler{NewTextHandler(bot, db)}
+}
+
+type rootHandler struct {
+	handler Handler
+}
+
+func (handler *rootHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
+	if update.Message != nil && update.Message.Chat != nil {
+		return handler.handler.HandleUpdate(update)
+	}
+	return log.Fields{"handler": "root"}, nil
+}
