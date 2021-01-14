a12129ff6181e09c54bd1e6bc7098ac49a4ab252
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-14 14:12:06 +0200

    Added text message handler

---
+++ internal/telegram/text_handler.go
@@ -0,0 +1,25 @@
+package telegram
+
+import (
+	"database/sql"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	log "github.com/sirupsen/logrus"
+)
+
+func NewTextHandler(bot *tgbotapi.BotAPI, db *sql.DB) Handler {
+	handlers := make(map[string]Handler)
+	handlers["/start"] = NewStartHandler(bot)
+	handlers["Зрозуміло \U0001F44D"] = handlers["/start"]
+	return &textHandler{handlers}
+}
+
+type textHandler struct {
+	handlers map[string]Handler
+}
+
+func (h *textHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
+	if handler, ok := h.handlers[update.Message.Text]; ok {
+		return handler.HandleUpdate(update)
+	}
+	return log.Fields{"handler": "text"}, nil
+}
