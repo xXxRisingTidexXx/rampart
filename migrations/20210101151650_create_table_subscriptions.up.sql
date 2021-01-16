7788aabdd6844e641c16cacfda092584ed0e7b45
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-16 16:55:16 +0200

    Added default enum values

--- migrations/20210101151650_create_table_subscriptions.up.sql
+++ migrations/20210101151650_create_table_subscriptions.up.sql
@@ -2,7 +2,7 @@ create table if not exists subscriptions
 (
     id          serial primary key not null,
     chat_id     bigint             not null,
-    status      varchar(10)        not null check ( status != '' ),
+    status      varchar(10)        not null check ( status != '' ) default 'actual',
     city        varchar(50)        not null check ( city != '' ),
     price       real               not null check ( 0 <= price ),
     room_number varchar(10)        not null check ( room_number != '' ),
--- migrations/20210109194123_create_table_transients.up.sql
+++ migrations/20210109194123_create_table_transients.up.sql
@@ -1,9 +1,9 @@
 create table if not exists transients
 (
     id          bigint primary key not null,
-    status      varchar(15)        not null check ( status != '' ),
+    status      varchar(15)        not null check ( status != '' )      default 'city',
     city        varchar(50)        not null check ( city != '' ),
-    price       real               not null check ( 0 <= price ),
-    room_number varchar(10)        not null check ( room_number != '' ),
-    floor       varchar(10)        not null check ( floor != '' )
+    price       real               not null check ( 0 <= price )        default 0,
+    room_number varchar(10)        not null check ( room_number != '' ) default 'any',
+    floor       varchar(10)        not null check ( floor != '' )       default 'any'
 );
