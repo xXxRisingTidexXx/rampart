a2a407dbdccc3e75ce22a19eb2029171e0928a33
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-18 01:13:52 +0200

    Added subscription rendering

--- internal/telegram/list_handler.go
+++ internal/telegram/list_handler.go
@@ -2,6 +2,7 @@ package telegram
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
@@ -18,6 +19,47 @@ type listHandler struct {
 
 func (h *listHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
 	fields := log.Fields{"handler": "list"}
-
+	tx, err := h.db.Begin()
+	if err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to begin a transaction, %v", err)
+	}
+	rows, err := tx.Query(
+		`select city, price, room_number, floor
+		from subscriptions
+		where status = 'actual'
+			and chat_id = $1`,
+		update.Message.Chat.ID,
+	)
+	if err != nil {
+		_ = tx.Rollback()
+		return fields, fmt.Errorf("telegram: handler failed to read subscriptions, %v", err)
+	}
+	for rows.Next() {
+		var subscription Subscription
+		err := rows.Scan(
+			&subscription.City,
+			&subscription.Price,
+			&subscription.RoomNumber,
+			&subscription.Floor,
+		)
+		if err != nil {
+			_ = rows.Close()
+			_ = tx.Rollback()
+			return fields, fmt.Errorf("telegram: handler failed to scan a row, %v", err)
+		}
+		log.Info(subscription)
+	}
+	if err := rows.Err(); err != nil {
+		_ = rows.Close()
+		_ = tx.Rollback()
+		return fields, fmt.Errorf("telegram: handler failed to finish iteration, %v", err)
+	}
+	if err := rows.Close(); err != nil {
+		_ = tx.Rollback()
+		return fields, fmt.Errorf("telegram: handler failed to close rows, %v", err)
+	}
+	if err := tx.Commit(); err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
+	}
 	return fields, nil
 }
---
+++ internal/telegram/subscription.go
@@ -0,0 +1,8 @@
+package telegram
+
+type Subscription struct {
+	City       string
+	Price      float32
+	RoomNumber string
+	Floor      string
+}
