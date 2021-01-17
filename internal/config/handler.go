80c4ae1f78ce1db896d70882f5e5ed14f93cdb5d
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-17 20:42:49 +0200

    Injected yaml fields

--- internal/config/handler.go
+++ internal/config/handler.go
@@ -1,5 +1,22 @@
 package config
 
 type Handler struct {
-	TemplatePath string `yaml:"template-path"`
+	StartCommand          string `yaml:"start-command"`
+	StartButton           string `yaml:"start-button"`
+	HelpCommand           string `yaml:"help-command"`
+	HelpButton            string `yaml:"help-button"`
+	CancelButton          string `yaml:"cancel-button"`
+	AddButton             string `yaml:"add-button"`
+	AnyPriceButton        string `yaml:"any-price-button"`
+	AnyRoomNumberButton   string `yaml:"any-room-number-button"`
+	OneRoomNumberButton   string `yaml:"one-room-number-button"`
+	TwoRoomNumberButton   string `yaml:"two-room-number-button"`
+	ThreeRoomNumberButton string `yaml:"three-room-number-button"`
+	ManyRoomNumberButton  string `yaml:"many-room-number-button"`
+	AnyFloorButton        string `yaml:"any-floor-button"`
+	LowFloorButton        string `yaml:"low-floor-button"`
+	HighFloorButton       string `yaml:"high-floor-button"`
+	TemplatePath          string `yaml:"template-path"`
+	MinFlatCount          int    `yaml:"min-flat-count"`
+	MaxRoomNumber         int64  `yaml:"max-room-number"`
 }
