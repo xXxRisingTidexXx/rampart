e75cc1005de27a04604877ac1a992760031fcf61
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-29 23:22:01 +0200

    Added uuid generation on subscription creation

--- internal/telegram/floor_status_handler.go
+++ internal/telegram/floor_status_handler.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/gofrs/uuid"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
 )
@@ -40,18 +41,24 @@ func (h *floorStatusHandler) HandleStatusUpdate(
 	if !ok {
 		return h.helper.prepareMessage(update, "invalid_floor", nil)
 	}
+	u, err := uuid.NewV4()
 	var message tgbotapi.MessageConfig
-	_, err := tx.Exec(
+	if err != nil {
+		return message, fmt.Errorf("telegram: handler failed to generate a uuid v4, %v", err)
+	}
+	_, err = tx.Exec(
 		`insert into subscriptions
-		(chat_id, city, price, room_number, floor)
+		(uuid, chat_id, city, price, room_number, floor)
 		values
 		(
 			$1,
-			(select city from transients where id = $1),
-			(select price from transients where id = $1),
-			(select room_number from transients where id = $1),
-			$2
+		 	$2,
+			(select city from transients where id = $2),
+			(select price from transients where id = $2),
+			(select room_number from transients where id = $2),
+			$3
 		)`,
+		u.String(),
 		update.Message.Chat.ID,
 		floor.String(),
 	)
