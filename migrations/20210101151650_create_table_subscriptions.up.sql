3100db0bea620f2045021f5b65b6d0d2ed774693
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-30 00:11:29 +0200

    Changed uuid type into string

--- migrations/20210101151650_create_table_subscriptions.up.sql
+++ migrations/20210101151650_create_table_subscriptions.up.sql
@@ -1,7 +1,7 @@
 create table if not exists subscriptions
 (
     id          serial primary key not null,
-    uuid        uuid unique        not null,
+    uuid        varchar(22) unique not null check ( uuid != '' ),
     chat_id     bigint             not null,
     status      varchar(10)        not null check ( status != '' ) default 'actual',
     city        varchar(50)        not null check ( city != '' ),
