99a7876c65fdd502fd1a626235df383c9ec79503
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-03 00:44:41 +0200

    Added snitch entrypoint

---
+++ cmd/snitch/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	log "github.com/sirupsen/logrus"
+	"github.com/xXxRisingTidexXx/rampart/internal/config"
+	"github.com/xXxRisingTidexXx/rampart/internal/metrics"
+)
+
+func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetReportCaller(true)
+	entry := log.WithField("app", "snitch")
+	if err := tgbotapi.SetLogger(entry); err != nil {
+		entry.Fatalf("main: snitch failed to set the logger, %v", err)
+	}
+	c, err := config.NewConfig()
+	if err != nil {
+		entry.Fatal(err)
+	}
+	db, err := sql.Open("postgres", c.Telegram.DSN)
+	if err != nil {
+		entry.Fatalf("main: snitch failed to open the db, %v", err)
+	}
+	if err := db.Ping(); err != nil {
+		_ = db.Close()
+		entry.Fatalf("main: snitch failed to ping the db, %v", err)
+	}
+	metrics.RunServer(c.Telegram.Server, entry)
+	if err = db.Close(); err != nil {
+		entry.Fatalf("main: snitch failed to close the db, %v", err)
+	}
+}
