57990b49d3522995dc70c3ac0346fa64d39be29e
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-04 14:27:37 +0200

    Added tgbotapi into publisher

--- internal/telegram/publisher.go
+++ internal/telegram/publisher.go
@@ -2,6 +2,8 @@ package telegram
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 )
@@ -11,6 +13,10 @@ func NewPublisher(
 	db *sql.DB,
 	logger log.FieldLogger,
 ) (*Publisher, error) {
+	bot, err := tgbotapi.NewBotAPI(config.Token)
+	if err != nil {
+		return nil, fmt.Errorf("telegram: publisher failed to instantiate, %v", err)
+	}
 	return &Publisher{logger}, nil
 }
 
