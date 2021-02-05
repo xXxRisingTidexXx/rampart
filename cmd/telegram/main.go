6e403768986fc9055e159c531c292b268ba2edd4
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-06 01:34:36 +0200

    Configured telegram entrypoint

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	_ "github.com/lib/pq"
+	"github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 	"github.com/xXxRisingTidexXx/rampart/internal/metrics"
@@ -14,6 +16,8 @@ import (
 // TODO: viber integration, https://github.com/mileusna/viber .
 // TODO: max connections, https://www.alexedwards.net/blog/configuring-sqldb .
 func main() {
+	isPublisher := flag.Bool("publisher", false, "Run just lookup worker & exit")
+	flag.Parse()
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
 	entry := log.WithField("app", "telegram")
@@ -35,10 +39,21 @@ func main() {
 	bot, err := tgbotapi.NewBotAPI(c.Telegram.Token)
 	if err != nil {
 		_ = db.Close()
-		entry.Fatalf("main: telegram failed to create a bot, %v", err)
+		entry.Fatalf("main: telegram failed to create the bot, %v", err)
 	}
-	metrics.RunServer(c.Telegram.Server, entry)
-	telegram.StartDispatcher(c.Telegram.Dispatcher, bot, db, entry)
+	publisher := telegram.NewPublisher(c.Telegram.Publisher, bot, db, entry)
+	if *isPublisher {
+		publisher.Run()
+	} else {
+		scheduler := cron.New()
+		if _, err := scheduler.AddJob(c.Telegram.Spec, publisher); err != nil {
+			_ = db.Close()
+			entry.Fatalf("main: telegram failed to run the publisher, %v", err)
+		}
+		scheduler.Start()
+		metrics.RunServer(c.Telegram.Server, entry)
+		telegram.StartDispatcher(c.Telegram.Dispatcher, bot, db, entry)
+	}
 	if err = db.Close(); err != nil {
 		entry.Fatalf("main: telegram failed to close the db, %v", err)
 	}
