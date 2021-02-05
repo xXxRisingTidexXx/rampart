62b9034ce8d5b7053b481edcfb3aafc1a985e7c8
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-06 01:26:54 +0200

    Changed field order

--- internal/config/telegram.go
+++ internal/config/telegram.go
@@ -3,8 +3,8 @@ package config
 type Telegram struct {
 	DSN        string     `yaml:"-"`
 	Token      string     `yaml:"-"`
-	Dispatcher Dispatcher `yaml:"dispatcher"`
 	Publisher  Publisher  `yaml:"publisher"`
 	Spec       string     `yaml:"spec"`
 	Server     Server     `yaml:"server"`
+	Dispatcher Dispatcher `yaml:"dispatcher"`
 }
