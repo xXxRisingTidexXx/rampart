d7c07500ca9f07dbf9016cef765920cae7208fa9
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-06 15:39:58 +0200

    Removed redundant columns and renamed image label into interior

--- migrations/20201016223044_create_table_images.up.sql
+++ migrations/20201016223044_create_table_images.up.sql
@@ -1,10 +1,10 @@
 create table if not exists images
 (
-    id      serial primary key not null,
-    flat_id integer            not null,
-    url     varchar(256)       not null check ( url != '' ),
-    kind    varchar(10)        not null check ( kind != '' ),
-    label   varchar(15)        not null check ( label != '' ) default 'unknown',
+    id       serial primary key not null,
+    flat_id  integer            not null,
+    url      varchar(256)       not null check ( url != '' ),
+    kind     varchar(10)        not null check ( kind != '' ),
+    interior varchar(15)        not null check ( interior != '' ) default 'unknown',
     foreign key (flat_id) references flats (id) on delete cascade,
     unique (flat_id, url)
 );
