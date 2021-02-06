478068b9ea662686a04b5978f0a8535ebed393e2
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-06 02:35:57 +0200

    Added liking preparation

--- internal/telegram/like_handler.go
+++ internal/telegram/like_handler.go
@@ -2,9 +2,12 @@ package telegram
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
+	"strconv"
+	"strings"
 )
 
 func NewLikeHandler(config config.Handler, bot *tgbotapi.BotAPI, db *sql.DB) Handler {
@@ -17,6 +20,23 @@ type likeHandler struct {
 	separator string
 }
 
-func (h *likeHandler) HandleUpdate(_ tgbotapi.Update) (log.Fields, error) {
-	return log.Fields{"handler": "like"}, nil
+func (h *likeHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
+	fields := log.Fields{"handler": "like"}
+	_, err := strconv.ParseInt(
+		update.CallbackQuery.Data[strings.LastIndex(update.CallbackQuery.Data, h.separator)+1:],
+		10,
+		64,
+	)
+	if err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to parse id, %v", err)
+	}
+	tx, err := h.db.Begin()
+	if err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to begin a transaction, %v", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
+	}
+	return fields, nil
 }
