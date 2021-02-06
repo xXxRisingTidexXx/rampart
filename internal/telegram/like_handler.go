ffc2b8b0d270e1bcf703b9c6997b8044f075af5b
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-06 03:24:26 +0200

    Almost finished like handler

--- internal/telegram/like_handler.go
+++ internal/telegram/like_handler.go
@@ -20,9 +20,10 @@ type likeHandler struct {
 	separator string
 }
 
+// TODO: add templates.
 func (h *likeHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
 	fields := log.Fields{"handler": "like"}
-	_, err := strconv.ParseInt(
+	id, err := strconv.ParseInt(
 		update.CallbackQuery.Data[strings.LastIndex(update.CallbackQuery.Data, h.separator)+1:],
 		10,
 		64,
@@ -30,13 +31,54 @@ func (h *likeHandler) HandleUpdate(update tgbotapi.Update) (log.Fields, error) {
 	if err != nil {
 		return fields, fmt.Errorf("telegram: handler failed to parse id, %v", err)
 	}
+	fields["id"] = id
 	tx, err := h.db.Begin()
 	if err != nil {
 		return fields, fmt.Errorf("telegram: handler failed to begin a transaction, %v", err)
 	}
-
+	fields["reaction"] = "positive"
+	ok, err := h.updateLookup(tx, id, true)
+	if err != nil {
+		_ = tx.Rollback()
+		return fields, err
+	}
+	if ok {
+		if err := tx.Commit(); err != nil {
+			return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
+		}
+		return fields, h.helper.answerCallback(update.CallbackQuery.ID, "positive_reaction")
+	}
+	fields["reaction"] = "negative"
+	ok, err = h.updateLookup(tx, id, false)
+	if err != nil {
+		_ = tx.Rollback()
+		return fields, err
+	}
+	if ok {
+		if err := tx.Commit(); err != nil {
+			return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
+		}
+		return fields, h.helper.answerCallback(update.CallbackQuery.ID, "negative_reaction")
+	}
+	fields["reaction"] = "absent"
 	if err := tx.Commit(); err != nil {
 		return fields, fmt.Errorf("telegram: handler failed to commit a transaction, %v", err)
 	}
-	return fields, nil
+	return fields, h.helper.answerCallback(update.CallbackQuery.ID, "absent_reaction")
+}
+
+func (h *likeHandler) updateLookup(tx *sql.Tx, id int64, shouldLike bool) (bool, error) {
+	result, err := tx.Exec(
+		`update lookups set is_liked = $1 where id = $2 and is_liked = not $1`,
+		shouldLike,
+		id,
+	)
+	if err != nil {
+		return false, fmt.Errorf("telegram: handler failed to update a lookup, %v", err)
+	}
+	number, err := result.RowsAffected()
+	if err != nil {
+		return false, fmt.Errorf("telegram: handler failed to get affected row number, %v", err)
+	}
+	return number > 0, nil
 }
