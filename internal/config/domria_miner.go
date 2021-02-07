0c94cda748426d07168124ef5aa0db8223708782
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-07 14:18:07 +0200

    Designed domria miner interface

--- internal/config/domria_miner.go
+++ internal/config/domria_miner.go
@@ -1,6 +1,14 @@
 package config
 
+import (
+	"time"
+)
+
 type DomriaMiner struct {
-	Name string `yaml:"name"`
-	Spec string `yaml:"spec"`
+	Name         string        `yaml:"name"`
+	Spec         string        `yaml:"spec"`
+	Timeout      time.Duration `yaml:"timeout"`
+	RetryLimit   int           `yaml:"retry-limit"`
+	SearchPrefix string        `yaml:"search-prefix"`
+	UserAgent    string        `yaml:"user-agent"`
 }
--- internal/config/storer.go
+++
@@ -1,5 +0,0 @@
-package config
-
-type Storer struct {
-	SRID int `yaml:"srid"`
-}
