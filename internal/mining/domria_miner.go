a014f57cfe013fecdb2e4cbc366240410971cad1
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-07 14:23:15 +0200

    Moved domria miner into mining

---
+++ internal/mining/domria_miner.go
@@ -0,0 +1,38 @@
+package mining
+
+import (
+	"github.com/xXxRisingTidexXx/rampart/internal/config"
+	"net/http"
+)
+
+func NewDomriaMiner(config config.DomriaMiner) Miner {
+	return &domriaMiner{
+		config.Name,
+		config.Spec,
+		&http.Client{Timeout: config.Timeout},
+		config.RetryLimit,
+		config.SearchPrefix,
+		config.UserAgent,
+	}
+}
+
+type domriaMiner struct {
+	name         string
+	spec         string
+	client       *http.Client
+	retryLimit   int
+	searchPrefix string
+	userAgent    string
+}
+
+func (m *domriaMiner) Name() string {
+	return m.name
+}
+
+func (m *domriaMiner) Spec() string {
+	return m.spec
+}
+
+func (m *domriaMiner) MineFlat() (Flat, error) {
+	return Flat{}, nil
+}
