b3caaeb1762ba52803b9271fd334ef7ef26f0301
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-07 23:54:49 +0200

    Added sanitation config sections

--- internal/config/domria_miner.go
+++ internal/config/domria_miner.go
@@ -1,14 +1,22 @@
 package config
 
 import (
+	"github.com/xXxRisingTidexXx/rampart/internal/misc"
 	"time"
 )
 
 type DomriaMiner struct {
-	Name         string        `yaml:"name"`
-	Spec         string        `yaml:"spec"`
-	Timeout      time.Duration `yaml:"timeout"`
-	RetryLimit   int           `yaml:"retry-limit"`
-	SearchPrefix string        `yaml:"search-prefix"`
-	UserAgent    string        `yaml:"user-agent"`
+	Name                   string            `yaml:"name"`
+	Spec                   string            `yaml:"spec"`
+	Timeout                time.Duration     `yaml:"timeout"`
+	RetryLimit             int               `yaml:"retry-limit"`
+	SearchPrefix           string            `yaml:"search-prefix"`
+	UserAgent              string            `yaml:"user-agent"`
+	URLPrefix              string            `yaml:"url-prefix"`
+	ImageURLFormat         string            `yaml:"image-url-format"`
+	Swaps                  misc.Set          `yaml:"swaps"`
+	CityOrthography        map[string]string `yaml:"city-orthography"`
+	StreetOrthography      []string          `yaml:"street-orthography"`
+	HouseNumberOrthography []string          `yaml:"house-number-orthography"`
+	HouseNumberMaxLength   int               `yaml:"house-number-max-length"`
 }
