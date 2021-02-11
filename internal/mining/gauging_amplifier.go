ed86aba4514c9bc669b2fb5aafec439166dccd4e
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-12 01:16:03 +0200

    Added gauging methods

--- internal/mining/gauging_amplifier.go
+++ internal/mining/gauging_amplifier.go
@@ -1,12 +1,22 @@
 package mining
 
 import (
+	"encoding/xml"
+	"fmt"
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/geo"
+	"github.com/paulmach/orb/geojson"
+	"github.com/paulmach/osm"
+	"github.com/paulmach/osm/osmgeojson"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
 	"github.com/xXxRisingTidexXx/rampart/internal/misc"
+	"math"
 	"net/http"
+	"net/url"
 )
 
 // TODO: relative city center distance feature (with city diameter).
+// TODO: metrics.
 func NewGaugingAmplifier(config config.GaugingAmplifier) Amplifier {
 	return &gaugingAmplifier{
 		&http.Client{Timeout: config.Timeout},
@@ -50,5 +60,193 @@ type gaugingAmplifier struct {
 }
 
 func (a *gaugingAmplifier) AmplifyFlat(flat Flat) (Flat, error) {
+	if !flat.HasLocation() {
+		return flat, nil
+	}
+
 	return flat, nil
+}
+
+func (a *gaugingAmplifier) gaugeSSF(flat Flat) (float64, error) {
+	if !a.subwayCities.Contains(flat.City) {
+		return 0, nil
+	}
+	collection, err := a.queryOverpass(
+		fmt.Sprintf(
+			"node[station=subway](around:%f,%f,%f);out;",
+			a.ssfSearchRadius,
+			flat.Point.Lat(),
+			flat.Point.Lon(),
+		),
+	)
+	if err != nil {
+		return 0, err
+	}
+	ssf := 0.0
+	for _, feature := range collection.Features {
+		distance := a.gaugeGeoDistance(feature.Geometry, flat.Point)
+		if distance != a.unknownDistance {
+			ssf += 1 / math.Max(distance, a.ssfMinDistance)
+		}
+	}
+	return ssf * a.ssfModifier, nil
+}
+
+func (a *gaugingAmplifier) queryOverpass(query string) (*geojson.FeatureCollection, error) {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		fmt.Sprintf(a.interpreterFormat, a.host, url.QueryEscape(query)),
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("mining: amplifier failed to construct a request, %v", err)
+	}
+	request.Header.Set("User-Agent", a.userAgent)
+	response, err := a.client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("mining: amplifier failed to make a request, %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		_ = response.Body.Close()
+		return nil, fmt.Errorf("mining: amplifier got response with status %s", response.Status)
+	}
+	var o osm.OSM
+	if err := xml.NewDecoder(response.Body).Decode(&o); err != nil {
+		_ = response.Body.Close()
+		return nil, fmt.Errorf("mining: amplifier failed to unmarshal xml, %v", err)
+	}
+	if err := response.Body.Close(); err != nil {
+		return nil, fmt.Errorf("mining: amplifier failed to close the response body, %v", err)
+	}
+	collection, err := osmgeojson.Convert(&o, osmgeojson.NoMeta(true))
+	if err != nil {
+		return nil, fmt.Errorf("mining: amplifier failed to convert from osm to geojson, %v", err)
+	}
+	return collection, nil
+}
+
+func (a *gaugingAmplifier) gaugeGeoDistance(geometry orb.Geometry, point orb.Point) float64 {
+	distance := a.unknownDistance
+	switch geometry := geometry.(type) {
+	case nil:
+		return distance
+	case orb.Point:
+		return geo.DistanceHaversine(geometry, point)
+	case orb.MultiPoint:
+		return a.gaugeGeoDistanceToPoints(geometry, point)
+	case orb.LineString:
+		return a.gaugeGeoDistanceToPoints(geometry, point)
+	case orb.MultiLineString:
+		distance := a.unknownDistance
+		for _, lineString := range geometry {
+			newDistance := a.gaugeGeoDistanceToPoints(lineString, point)
+			if a.isLower(newDistance, distance) {
+				distance = newDistance
+			}
+		}
+		return distance
+	case orb.Ring:
+		return a.gaugeGeoDistanceToPoints(geometry, point)
+	case orb.Polygon:
+		for _, ring := range geometry {
+			newDistance := a.gaugeGeoDistanceToPoints(ring, point)
+			if a.isLower(newDistance, distance) {
+				distance = newDistance
+			}
+		}
+		return distance
+	case orb.MultiPolygon:
+		for _, polygon := range geometry {
+			for _, ring := range polygon {
+				newDistance := a.gaugeGeoDistanceToPoints(ring, point)
+				if a.isLower(newDistance, distance) {
+					distance = newDistance
+				}
+			}
+		}
+		return distance
+	case orb.Collection:
+		for _, newGeometry := range geometry {
+			newDistance := a.gaugeGeoDistance(newGeometry, point)
+			if a.isLower(newDistance, distance) {
+				distance = newDistance
+			}
+		}
+		return distance
+	case orb.Bound:
+		return a.gaugeGeoDistanceToPoints(geometry.ToRing(), point)
+	default:
+		return distance
+	}
+}
+
+func (a *gaugingAmplifier) isLower(d1, d2 float64) bool {
+	return d1 < d2 || d2 == a.unknownDistance && d2 < d1
+}
+
+func (a *gaugingAmplifier) gaugeGeoDistanceToPoints(points []orb.Point, point orb.Point) float64 {
+	distance := a.unknownDistance
+	for _, newPoint := range points {
+		newDistance := geo.DistanceHaversine(newPoint, point)
+		if a.isLower(newDistance, distance) {
+			distance = newDistance
+		}
+	}
+	return distance
+}
+
+func (a *gaugingAmplifier) gaugeIZF(flat Flat) (float64, error) {
+	collection, err := a.queryOverpass(
+		fmt.Sprintf(
+			"(way[landuse=industrial](around:%f,%f,%f);>;relation[landuse=industrial](around:%f,%"+
+				"f,%f);>;);out;",
+			a.izfSearchRadius,
+			flat.Point.Lat(),
+			flat.Point.Lon(),
+			a.izfSearchRadius,
+			flat.Point.Lat(),
+			flat.Point.Lon(),
+		),
+	)
+	if err != nil {
+		return 0, err
+	}
+	izf := 0.0
+	for _, feature := range collection.Features {
+		if area := geo.Area(feature.Geometry); area >= a.izfMinArea {
+			distance := a.gaugeGeoDistance(feature.Geometry, flat.Point)
+			if distance != a.unknownDistance {
+				izf += area / math.Max(distance, a.izfMinDistance)
+			}
+		}
+	}
+	return izf * a.izfModifier, nil
+}
+
+func (a *gaugingAmplifier) gaugeGZF(flat Flat) (float64, error) {
+	collection, err := a.queryOverpass(
+		fmt.Sprintf(
+			"(way[leisure=park](around:%f,%f,%f);>;relation[leisure=park](around:%f,%f,%f);>;);ou"+
+				"t;",
+			a.gzfSearchRadius,
+			flat.Point.Lat(),
+			flat.Point.Lon(),
+			a.gzfSearchRadius,
+			flat.Point.Lat(),
+			flat.Point.Lon(),
+		),
+	)
+	if err != nil {
+		return 0, err
+	}
+	gzf := 0.0
+	for _, feature := range collection.Features {
+		if area := geo.Area(feature.Geometry); area >= a.gzfMinArea {
+			distance := a.gaugeGeoDistance(feature.Geometry, flat.Point)
+			if distance != a.unknownDistance {
+				gzf += area / math.Max(distance, a.gzfMinDistance)
+			}
+		}
+	}
+	return gzf * a.gzfModifier, nil
 }
