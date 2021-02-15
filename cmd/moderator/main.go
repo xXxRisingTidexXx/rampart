be97df3370264bf40d433f61dd56a0c630fd9fa4
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-16 01:02:36 +0200

    Added graceful shutdown to moderator

--- cmd/moderator/main.go
+++ cmd/moderator/main.go
@@ -6,6 +6,9 @@ import (
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/rampart/internal/config"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -19,7 +22,7 @@ func main() {
 	if err != nil {
 		entry.Fatal(err)
 	}
-	db, err := sql.Open("postgres", c.Telegram.DSN)
+	db, err := sql.Open("postgres", c.Moderator.DSN)
 	if err != nil {
 		entry.Fatalf("main: moderator failed to open the db, %v", err)
 	}
@@ -27,12 +30,15 @@ func main() {
 		_ = db.Close()
 		entry.Fatalf("main: moderator failed to ping the db, %v", err)
 	}
-	_, err = tgbotapi.NewBotAPI(c.Telegram.Token)
+	_, err = tgbotapi.NewBotAPI(c.Moderator.Token)
 	if err != nil {
 		_ = db.Close()
 		entry.Fatalf("main: moderator failed to create the bot, %v", err)
 	}
 
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	<-signals
 	if err = db.Close(); err != nil {
 		entry.Fatalf("main: moderator failed to close the db, %v", err)
 	}
