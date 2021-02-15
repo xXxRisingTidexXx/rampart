53e5afe286e3033ac1e0df45960f8c79289b2075
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-15 23:58:27 +0200

    Renamed telegram entrypoint into assistant

--- cmd/assistant/main.go
+++ cmd/assistant/main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/xXxRisingTidexXx/rampart/internal/telegram"
 )
 
-// TODO: signal handling & graceful shutdown.
 // TODO: viber integration, https://github.com/mileusna/viber .
 // TODO: max connections, https://www.alexedwards.net/blog/configuring-sqldb .
 func main() {
@@ -20,9 +19,9 @@ func main() {
 	flag.Parse()
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetReportCaller(true)
-	entry := log.WithField("app", "telegram")
+	entry := log.WithField("app", "assistant")
 	if err := tgbotapi.SetLogger(entry); err != nil {
-		entry.Fatalf("main: telegram failed to set the logger, %v", err)
+		entry.Fatalf("main: assistant failed to set the logger, %v", err)
 	}
 	c, err := config.NewConfig()
 	if err != nil {
@@ -30,16 +29,16 @@ func main() {
 	}
 	db, err := sql.Open("postgres", c.Telegram.DSN)
 	if err != nil {
-		entry.Fatalf("main: telegram failed to open the db, %v", err)
+		entry.Fatalf("main: assistant failed to open the db, %v", err)
 	}
 	if err := db.Ping(); err != nil {
 		_ = db.Close()
-		entry.Fatalf("main: telegram failed to ping the db, %v", err)
+		entry.Fatalf("main: assistant failed to ping the db, %v", err)
 	}
 	bot, err := tgbotapi.NewBotAPI(c.Telegram.Token)
 	if err != nil {
 		_ = db.Close()
-		entry.Fatalf("main: telegram failed to create the bot, %v", err)
+		entry.Fatalf("main: assistant failed to create the bot, %v", err)
 	}
 	publisher := telegram.NewPublisher(c.Telegram.Publisher, bot, db, entry)
 	if *isPublisher {
@@ -48,13 +47,13 @@ func main() {
 		scheduler := cron.New()
 		if _, err := scheduler.AddJob(c.Telegram.Spec, publisher); err != nil {
 			_ = db.Close()
-			entry.Fatalf("main: telegram failed to run the publisher, %v", err)
+			entry.Fatalf("main: assitant failed to run the publisher, %v", err)
 		}
 		scheduler.Start()
 		metrics.RunServer(c.Telegram.Server, entry)
 		telegram.StartDispatcher(c.Telegram.Dispatcher, bot, db, entry)
 	}
 	if err = db.Close(); err != nil {
-		entry.Fatalf("main: telegram failed to close the db, %v", err)
+		entry.Fatalf("main: assistant failed to close the db, %v", err)
 	}
 }
