e3858f38bf26a2fa981a4e29b1cc87f244175690
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-17 00:51:39 +0200

    Added interior buttons

--- internal/config/moderator_handler.go
+++ internal/config/moderator_handler.go
@@ -1,8 +1,13 @@
 package config
 
 type ModeratorHandler struct {
-	Admin             string `yaml:"-"`
-	StartCommand      string `yaml:"start-command"`
-	ImageMarkupButton string `yaml:"image-markup-button"`
-	Separator         string `yaml:"separator"`
+	Admin              string `yaml:"-"`
+	StartCommand       string `yaml:"start-command"`
+	StartButton        string `yaml:"start-button"`
+	ImageMarkupButton  string `yaml:"image-markup-button"`
+	LuxuryButton       string `yaml:"luxury-button"`
+	ComfortButton      string `yaml:"comfort-button"`
+	JunkButton         string `yaml:"junk-button"`
+	ConstructionButton string `yaml:"construction-button"`
+	ExcessButton       string `yaml:"excess-button"`
 }
