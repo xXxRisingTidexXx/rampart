b764cf21b2654823e8c009325ef4e6d37c70d4bd
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-17 22:58:30 +0200

    Adjusted moderator button names

--- internal/config/moderator_handler.go
+++ internal/config/moderator_handler.go
@@ -3,11 +3,11 @@ package config
 type ModeratorHandler struct {
 	Admin              string `yaml:"-"`
 	StartCommand       string `yaml:"start-command"`
-	StartButton        string `yaml:"start-button"`
-	ImageMarkupButton  string `yaml:"image-markup-button"`
+	MarkupButton       string `yaml:"markup-button"`
 	LuxuryButton       string `yaml:"luxury-button"`
 	ComfortButton      string `yaml:"comfort-button"`
 	JunkButton         string `yaml:"junk-button"`
 	ConstructionButton string `yaml:"construction-button"`
 	ExcessButton       string `yaml:"excess-button"`
+	EnoughButton       string `yaml:"enough-button"`
 }
