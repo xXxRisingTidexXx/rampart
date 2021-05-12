18feae6860ab6f2bc4c9ef50fd618c33509434b6
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-05-13 01:12:34 +0300

    Some web

--- rampart/classification.py
+++ rampart/classification.py
@@ -1,66 +1,96 @@
-from typing import List
 from enum import Enum, unique
 from lightgbm import Booster
+from numpy import ndarray
 from pandas import read_sql, DataFrame
 from sqlalchemy.engine.base import Engine
 
 
-class Classifier:
-    __slots__ = ['_loader', '_reader', '_booster', '_writer', '_limit']
-
-    def __init__(self, engine: Engine):
-        self._loader = Loader(engine)
-        self._reader = Reader(engine)
-        self._booster = Booster(model_file=config.model_path)
-        self._writer = Writer(engine)
-
-    def __call__(self):
-        for subscription in self._loader.load_subscriptions():
-            flats = self._reader.read_flats(subscription)
-            if len(flats) > 0:
-                flats['label'] = (
-                    self._booster
-                        .predict(flats.drop(columns=['id']))
-                        .round(0)
-                        .astype(int)
-                )
-                ids = flats[flats['label'] == 1]['id'].tolist()
-                if len(ids) > 0:
-                    self._writer.write_lookup(Lookup(subscription.id, ids))
-
-
-class Loader:
+class Reader:
     __slots__ = ['_engine']
 
     def __init__(self, engine: Engine):
         self._engine = engine
 
-    def load_subscriptions(self) -> List['Subscription']:
+    def read_flats(self, query: 'Query') -> DataFrame:
         with self._engine.connect() as connection:
-            return [
-                Subscription(s[0], s[1], s[2], RoomNumber[s[3]], Floor[s[4]])
-                for s in connection.execute(
-                    '''
-                    select id, city, price, room_number, floor
-                    from subscriptions
-                    where status = 'active'
-                    '''
-                )
-            ]
+            return read_sql(
+                f'''
+                select flats.id,
+                    flats.url,
+                    price        as actual_price,
+                    %s           as utmost_price,
+                    total_area,
+                    living_area,
+                    kitchen_area,
+                    room_number  as actual_room_number,
+                    %s           as desired_room_number,
+                    floor        as actual_floor,
+                    total_floor,
+                    %s           as desired_floor,
+                    case
+                        when housing = 'primary' then 0
+                        else 1
+                        end      as housing,
+                    ssf,
+                    izf,
+                    gzf,
+                    sum(
+                        case
+                            when interior = 'abandoned' then 1
+                            else 0
+                            end) as abandoned_count,
+                    sum(
+                        case
+                            when interior = 'luxury' then 1
+                            else 0
+                            end) as luxury_count,
+                    sum(
+                        case
+                            when interior = 'comfort' then 1
+                            else 0
+                            end) as comfort_count,
+                    sum(
+                        case
+                            when interior = 'junk' then 1
+                            else 0
+                            end) as junk_count,
+                    sum(
+                        case
+                            when interior = 'construction' then 1
+                            else 0
+                            end) as construction_count,
+                    sum(
+                        case
+                            when interior = 'excess' then 1
+                            else 0
+                            end) as excess_count,
+                    0 as panorama_count
+                from flats
+                    join visuals on flats.id = visuals.flat_id
+                    join images on visuals.image_id = images.id
+                where city = %s and interior != 'unknown'
+                group by flats.id
+                ''',
+                connection,
+                params=[
+                    query.price,
+                    query.room_number.value,
+                    query.floor.value,
+                    query.city
+                ]
+            )
 
 
-class Subscription:
-    __slots__ = ['id', 'city', 'price', 'room_number', 'floor']
+class Query:
+    __slots__ = ['city', 'price', 'room_number', 'floor']
 
     def __init__(
         self,
-        id_: int,
         city: str,
         price: float,
         room_number: 'RoomNumber',
         floor: 'Floor'
     ):
-        self.id = id_
         self.city = city
         self.price = price
         self.room_number = room_number
@@ -83,117 +113,11 @@ class Floor(Enum):
     high = 2
 
 
-class Reader:
-    __slots__ = ['_engine']
+class Classifier:
+    __slots__ = ['_booster']
 
-    def __init__(self, engine: Engine):
-        self._engine = engine
+    def __init__(self):
+        self._booster = Booster(model_file='scientific/models/twinkle.latest.txt')
 
-    def read_flats(self, subscription: Subscription) -> DataFrame:
-        with self._engine.connect() as connection:
-            return read_sql(
-                f'''
-                select flats.id,
-                    price        as actual_price,
-                    %s           as utmost_price,
-                    total_area,
-                    living_area,
-                    kitchen_area,
-                    room_number  as actual_room_number,
-                    %s           as desired_room_number,
-                    floor        as actual_floor,
-                    total_floor,
-                    %s           as desired_floor,
-                    case
-                        when housing = 'primary' then 0
-                        else 1
-                        end      as housing,
-                    ssf,
-                    izf,
-                    gzf,
-                    sum(
-                        case
-                            when kind = 'photo'
-                                and label = 'abandoned' then 1
-                            else 0
-                            end) as abandoned_count,
-                    sum(
-                        case
-                            when kind = 'photo' and label = 'luxury' then 1
-                            else 0
-                            end) as luxury_count,
-                    sum(
-                        case
-                            when kind = 'photo' and label = 'comfort' then 1
-                            else 0
-                            end) as comfort_count,
-                    sum(
-                        case
-                            when kind = 'photo' and label = 'junk' then 1
-                            else 0
-                            end) as junk_count,
-                    sum(
-                        case
-                            when kind = 'photo'
-                                and label = 'construction' then 1
-                            else 0
-                            end) as construction_count,
-                    sum(
-                        case
-                            when kind = 'photo' and label = 'excess' then 1
-                            else 0
-                            end) as excess_count,
-                    sum(
-                        case
-                            when kind = 'panorama' then 1
-                            else 0
-                            end) as panorama_count
-                from flats
-                    join images on flats.id = flat_id
-                where city = %s
-                    and flats.id not in (
-                        select lookups.flat_id
-                        from lookups
-                        where subscription_id = %s)
-                group by flats.id
-                having sum(
-                    case
-                        when kind = 'photo' and label = 'unknown' then 1
-                        else 0
-                        end) = 0
-                ''',
-                connection,
-                params=[
-                    subscription.price,
-                    subscription.room_number.value,
-                    subscription.floor.value,
-                    subscription.city,
-                    subscription.id
-                ]
-            )
-
-
-class Writer:
-    __slots__ = ['_engine']
-
-    def __init__(self, engine: Engine):
-        self._engine = engine
-
-    def write_lookup(self, lookup: 'Lookup'):
-        with self._engine.begin() as connection:
-            connection.execute(
-                '''
-                insert into lookups (subscription_id, flat_id)
-                values (%s, %s)
-                returning id
-                ''',
-                *[(lookup.subscription_id, f) for f in lookup.flat_ids]
-            )
-
-
-class Lookup:
-    __slots__ = ['subscription_id', 'flat_ids']
-
-    def __init__(self, subscription_id: int, flat_ids: List[int]):
-        self.subscription_id = subscription_id
-        self.flat_ids = flat_ids
+    def classify_flats(self, flats: DataFrame) -> ndarray:
+        return self._booster.predict(flats).round(0).astype(bool)
--- rampart/web.py
+++ rampart/web.py
@@ -1,11 +1,34 @@
 from os import getenv
 from fastapi import FastAPI
 from sqlalchemy import create_engine
+from rampart.classification import Reader, Classifier, Query, RoomNumber, Floor
 
 app = FastAPI()
-_engine = create_engine(getenv('RAMPART_DSN'))
+_reader = Reader(create_engine(getenv('RAMPART_DSN')))
+_classifier = Classifier()
 
 
 @app.get('/')
-def _read_root():
-    return {'greeting': 'Great!'}
+def _read_root(
+    city: str = 'Київ',
+    price: float = 0,
+    room_number: str = 'any',
+    floor: str = 'any',
+):
+    if room_number not in RoomNumber.__members__:
+        room_number = 'any'
+    if floor not in Floor.__members__:
+        floor = 'any'
+    flats = _reader.read_flats(Query(city, price, RoomNumber[room_number], Floor[floor]))
+    if len(flats) <= 0:
+        return []
+    flats['is_relevant'] = _classifier.classify_flats(flats.drop(columns=['id', 'url']))
+    return [
+        {
+            'url': f['url'],
+            'price': f['actual_price'],
+            'room_number': f['actual_room_number'],
+            'floor': f['actual_floor']
+        }
+        for _, f in flats[flats['is_relevant']].iterrows()
+    ]
